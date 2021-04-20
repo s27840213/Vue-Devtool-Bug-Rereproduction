91667d98861a22d35af17457504834ec905dcfae
steve <nuphoto.steve@gmail.com>
2021-04-20 09:48:11 +0800

    Scaling feature: 85%, translation part of scaling done, the scaling part needs improved.

--- src/components/editor/global/NuController.vue
+++ src/components/editor/global/NuController.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
   div(class="nu-controller" ref="body"
   :style="styles()" @mousedown="moveStart")
-    div(v-for="(controlPoint,index) in controlPoints"
+    div(v-for="(controlPoint, index) in controlPoints.positions"
     class="scaler"
     :key="index"
-    :style="controlPoint.styles"
-    @mousedown="scaleStart($event, controlPoint.xSign, controlPoint.ySign)")
+    :style="Object.assign(controlPoint, cursorStyles(index, getLayerRotate))"
+    @mousedown="scaleStart")
     div(class="rotaterWrapper")
       div(class="rotater" @mousedown="rotateStart")
 </template>
@@ -25,21 +25,37 @@ export default Vue.extend({
       controlPoints: ControlPoints,
       initialX: 0,
       initialY: 0,
+      initWidth: 0,
+      initHeight: 0,
+      center: {
+        x: 0,
+        y: 0
+      },
+      initTranslate: {
+        x: 0,
+        y: 0
+      },
       translate: {
-        active: false,
-        xOffset: this.config.styles.x,
-        yOffset: this.config.styles.y
+        active: false
       },
       scale: {
-        eventHandler: {} as any
-      },
-      rotate: {
-        centerX: 0,
-        centerY: 0
+        xSign: 1,
+        ySign: 1,
+        anchorPoint: {
+          x: 0,
+          y: 0
+        },
+        initOrigin: {
+          x: 0,
+          y: 0
+        }
       }
     }
   },
   computed: {
+    getControlPoints() {
+      return this.config.controlPoints
+    },
     getLayerX() {
       return this.config.styles.x
     },
@@ -57,6 +73,18 @@ export default Vue.extend({
     }
   },
   methods: {
+    styles() {
+      return {
+        transform: `translate(${this.config.styles.x}px, ${this.config.styles.y}px) rotate(${this.config.styles.rotate}deg)`,
+        width: `${this.config.styles.width}px`,
+        height: `${this.config.styles.height}px`
+      }
+    },
+    cursorStyles(index: number, rotateAngle: number) {
+      const cursorIndex = rotateAngle >= 0 ? (index + Math.floor(rotateAngle / 45)) % 8
+        : (index + Math.ceil(rotateAngle / 45) + 8) % 8
+      return { cursor: this.controlPoints.cursors[cursorIndex] }
+    },
     ...mapMutations({
       Update_LayerPos: 'Update_LayerPos',
       Update_LayerSize: 'Update_LayerSize',
@@ -86,19 +114,11 @@ export default Vue.extend({
       })
     },
 
-    styles() {
-      return {
-        transform: `translate(${this.config.styles.x}px, ${this.config.styles.y}px) rotate(${this.config.styles.rotate}deg)`,
-        width: `${this.config.styles.width}px`,
-        height: `${this.config.styles.height}px`
-      }
-    },
-
     moveStart(event: MouseEvent) {
       this.initialX = event.clientX
       this.initialY = event.clientY
-      this.translate.xOffset = this.getLayerX
-      this.translate.yOffset = this.getLayerY
+      this.initTranslate.x = this.getLayerX
+      this.initTranslate.y = this.getLayerY
       if (event.target === this.$refs.body) {
         const el = event.target as HTMLElement
         el.addEventListener('mouseup', this.moveEnd)
@@ -110,8 +130,8 @@ export default Vue.extend({
       if (this.translate.active) {
         event.preventDefault()
         const moveOffset = PropsTransformer.getActualMoveOffset(event.clientX - this.initialX, event.clientY - this.initialY)
-        const x = moveOffset.offsetX + this.translate.xOffset
-        const y = moveOffset.offsetY + this.translate.yOffset
+        const x = moveOffset.offsetX + this.initTranslate.x
+        const y = moveOffset.offsetY + this.initTranslate.y
         this.updateLayerPos(0, 2, x, y)
       }
     },
@@ -123,40 +143,100 @@ export default Vue.extend({
       }
     },
 
-    scaleStart(event: MouseEvent, xSign: number, ySign: number) {
+    scaleStart(event: MouseEvent) {
       this.initialX = event.clientX
       this.initialY = event.clientY
-      this.scale.eventHandler = this.scaling(xSign, ySign)
-      document.documentElement.addEventListener('mousemove', this.scale.eventHandler, false)
+
+      const body = this.$el as HTMLElement
+      this.initWidth = parseInt(body.style.width, 10)
+      this.initHeight = parseInt(body.style.height, 10)
+
+      const rect = this.$el.getBoundingClientRect()
+      this.center = {
+        x: rect.left + rect.width / 2 - window.pageXOffset,
+        y: rect.top + rect.height / 2 - window.pageYOffset
+      }
+
+      this.scale.xSign = (event.clientX - this.center.x > 0) ? 1 : -1
+      this.scale.ySign = (event.clientY - this.center.y > 0) ? 1 : -1
+
+      this.scale.initOrigin.x = this.center.x - parseInt(body.style.width) / 2
+      this.scale.initOrigin.y = this.center.y - parseInt(body.style.height) / 2
+      this.initTranslate = {
+        x: this.getLayerX,
+        y: this.getLayerY
+      }
+
+      const angleInRad = this.getLayerRotate * Math.PI / 180
+      let vectX = event.clientX - this.center.x
+      let vectY = event.clientY - this.center.y
+
+      // Get client point while no rotation
+      const clientP = {
+        x: vectX * Math.cos(-angleInRad) - vectY * Math.sin(-angleInRad) + this.center.x,
+        y: vectY * Math.cos(-angleInRad) + vectX * Math.sin(-angleInRad) + this.center.y
+      }
+      const anchorP = {
+        x: clientP.x < this.center.x ? clientP.x + this.initWidth : clientP.x - this.initWidth,
+        y: clientP.y < this.center.y ? clientP.y + this.initHeight : clientP.y - this.initHeight
+      }
+      vectX = anchorP.x - this.center.x
+      vectY = anchorP.y - this.center.y
+      this.scale.anchorPoint = {
+        x: vectX * Math.cos(angleInRad) - vectY * Math.sin(angleInRad) + this.center.x,
+        y: vectY * Math.cos(angleInRad) + vectX * Math.sin(angleInRad) + this.center.y
+      }
+
+      document.documentElement.addEventListener('mousemove', this.scaling, false)
       document.documentElement.addEventListener('mouseup', this.scaleEnd, false)
     },
-    scaling(xSign: number, ySign: number) {
-      return (event: MouseEvent) => {
-        event.preventDefault()
-        const width = parseInt(this.getLayerWidth, 10) + xSign * event.movementX
-        const height = parseInt(this.getLayerHeight, 10) + ySign * event.movementY
-        if (width <= 20 || height <= 20) return
+    scaling(event: MouseEvent) {
+      event.preventDefault()
 
-        const x = xSign < 0 ? parseInt(this.getLayerX) + event.movementX : parseInt(this.getLayerX)
-        const y = ySign < 0 ? parseInt(this.getLayerY) + event.movementY : parseInt(this.getLayerY)
+      let width = parseInt(this.getLayerWidth, 10)
+      let height = parseInt(this.getLayerHeight, 10)
+      const offsetWidth = this.scale.xSign * (event.clientX - this.initialX)
+      const offsetHeight = this.scale.ySign * (event.clientY - this.initialY)
 
-        this.translate.xOffset += xSign < 0 ? event.movementX : 0
-        this.translate.yOffset += ySign < 0 ? event.movementY : 0
-
-        this.updateLayerSize(0, 2, width, height)
-        this.updateLayerPos(0, 2, x, y)
+      if ((width + offsetWidth) / this.initWidth >= (height + offsetHeight) / this.initHeight) {
+        width = offsetWidth + this.initWidth
+        height = width * this.initHeight / this.initWidth
+      } else {
+        height = offsetHeight + this.initHeight
+        width = height * this.initWidth / this.initHeight
       }
+      if (width <= 20 || height <= 20) return
+
+      const ratio = width / this.initWidth
+      const center = {
+        x: (this.center.x - this.scale.anchorPoint.x) * ratio + this.scale.anchorPoint.x,
+        y: (this.center.y - this.scale.anchorPoint.y) * ratio + this.scale.anchorPoint.y
+      }
+
+      // const anchorP = this.scale.anchorPoint
+      const origin = {
+        x: center.x - width / 2,
+        y: center.y - height / 2
+      }
+
+      const x = this.initTranslate.x + (origin.x - this.scale.initOrigin.x)
+      const y = this.initTranslate.y + (origin.y - this.scale.initOrigin.y)
+
+      this.updateLayerSize(0, 2, width, height)
+      this.updateLayerPos(0, 2, x, y)
     },
     scaleEnd() {
-      document.documentElement.removeEventListener('mousemove', this.scale.eventHandler, false)
+      document.documentElement.removeEventListener('mousemove', this.scaling, false)
       document.documentElement.removeEventListener('mouseup', this.scaleEnd, false)
     },
 
     rotateStart(event: MouseEvent) {
       const body = this.$el
       const rect = body.getBoundingClientRect()
-      this.rotate.centerX = rect.left + rect.width / 2 - window.pageXOffset
-      this.rotate.centerY = rect.top + rect.height / 2 - window.pageYOffset
+      this.center = {
+        x: rect.left + rect.width / 2 - window.pageXOffset,
+        y: rect.top + rect.height / 2 - window.pageYOffset
+      }
 
       this.initialX = event.clientX
       this.initialY = event.clientY
@@ -165,8 +245,8 @@ export default Vue.extend({
       window.addEventListener('mouseup', this.rotateEnd)
     },
     rotating(event: MouseEvent) {
-      const [lineAx, lineAy] = [this.initialX - this.rotate.centerX, this.initialY - this.rotate.centerY]
-      const [lineBx, lineBy] = [event.clientX - this.rotate.centerX, event.clientY - this.rotate.centerY]
+      const [lineAx, lineAy] = [this.initialX - this.center.x, this.initialY - this.center.y]
+      const [lineBx, lineBy] = [event.clientX - this.center.x, event.clientY - this.center.y]
 
       const lineA = Math.sqrt(Math.pow(lineAx, 2) + Math.pow(lineAy, 2))
       const lineB = Math.sqrt(Math.pow(lineBx, 2) + Math.pow(lineBy, 2))
--- src/store/types.ts
+++ src/store/types.ts
@@ -7,7 +7,7 @@ export interface IState {
 export interface IEditorState {
   pages: Array<IPage>,
   currPanelType: number,
-  pageScaleRatio: number
+  pageScaleRatio: number,
 }
 
 export enum PanelType {
@@ -33,72 +33,44 @@ export enum LayerType {
 }
 
 export const ControlPoints = {
-  ne: {
-    xSign: 1,
-    ySign: -1,
-    styles: {
-      right: '0px',
-      top: '0px',
-      cursor: 'nesw-resize'
-    }
-  },
-  nw: {
-    xSign: -1,
-    ySign: -1,
-    styles: {
+  positions: [
+    {
       left: '0px',
-      top: '0px',
-      cursor: 'nwse-resize'
-    }
-  },
-  se: {
-    xSign: 1,
-    ySign: 1,
-    styles: {
+      top: '0px'
+    },
+    {
+      top: '0px'
+    },
+    {
       right: '0px',
-      bottom: '0px',
-      cursor: 'nwse-resize'
-    }
-  },
-  sw: {
-    xSign: -1,
-    ySign: 1,
-    styles: {
-      left: '0px',
-      bottom: '0px',
-      cursor: 'nesw-resize'
-    }
-  },
-  s: {
-    xSign: 0,
-    ySign: 1,
-    styles: {
-      bottom: '0px',
-      cursor: 'ns-resize'
-    }
-  },
-  n: {
-    xSign: 0,
-    ySign: -1,
-    styles: {
-      top: '0px',
-      cursor: 'ns-resize'
-    }
-  },
-  e: {
-    xSign: 1,
-    ySign: 0,
-    styles: {
+      top: '0px'
+    },
+    {
+      right: '0px'
+    },
+    {
       right: '0px',
-      cursor: 'ew-resize'
-    }
-  },
-  w: {
-    xSign: -1,
-    ySign: 0,
-    styles: {
+      bottom: '0px'
+    },
+    {
+      bottom: '0px'
+    },
+    {
       left: '0px',
-      cursor: 'ew-resize'
+      bottom: '0px'
+    },
+    {
+      left: '0px'
     }
-  }
+  ],
+  cursors: [
+    'nwse-resize',
+    'ns-resize',
+    'nesw-resize',
+    'ew-resize',
+    'nwse-resize',
+    'ns-resize',
+    'nesw-resize',
+    'ew-resize'
+  ]
 }
