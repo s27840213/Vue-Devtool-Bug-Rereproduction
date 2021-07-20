2a1eeff1a2b8335bd63f010c975f003edaa0e6fc
giambihuang <nuphoto.giambi@gmail.com>
2021-07-20 15:46:22 +0800

    text effect: add the curve feature

--- src/components/editor/global/NuLayer.vue
+++ src/components/editor/global/NuLayer.vue
@@ -63,7 +63,6 @@ export default Vue.extend({
       const styles = this.config.type === 'text' ? Object.assign(CssConveter.convertDefaultStyle(this.config.styles),
         { background: 'rgba(0, 0, 255, 0)' }) : CssConveter.convertDefaultStyle(this.config.styles)
       if (this.config.type === 'text') {
-        styles.opacity = this.config.active ? '0' : '1'
         Object.assign(
           styles,
           TextEffectUtils.convertTextEffect(this.config.styles.textEffect || {})
