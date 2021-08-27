9836750333ee55995b67f890d6b27c43e218d11b
giambihuang <nuphoto.giambi@gmail.com>
2021-08-27 16:43:05 +0800

    error tracking: implement and configure Sentry

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,37 @@ import { Store } from 'vuex'
 import { IEditorState } from './store/types'
 import { RecycleScroller } from 'vue-virtual-scroller'
 
+import * as Sentry from '@sentry/vue'
+import { Integrations } from '@sentry/tracing'
+
 Vue.config.productionTip = false
 Vue.use(VueRecyclerviewNew, vueColor)
 Vue.component('RecycleScroller', RecycleScroller)
 const requireAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().map(requireContext)
 const req = require.context('@/assets/icon', true, /\.svg$/)
 requireAll(req)
+
+Sentry.init({
+  Vue,
+  trackComponents: true,
+  maxBreadcrumbs: 10,
+  tracesSampleRate: 1.0,
+  environment: process.env.NODE_ENV,
+  dsn: process.env.VUE_APP_SENTRY_DSN,
+  release: process.env.VUE_APP_SENTRY_RELEASE,
+  integrations: [
+    new Integrations.BrowserTracing({
+      routingInstrumentation: Sentry.vueRouterInstrumentation(router)
+    })
+  ],
+  beforeBreadcrumb (breadcrumb, hint) {
+    if (hint && breadcrumb.category && ['xhr'].includes(breadcrumb.category)) {
+      const { __sentry_xhr__: request, response } = hint.xhr
+      Object.assign(breadcrumb.data, { response, requestBody: request.body })
+    }
+    return breadcrumb
+  }
+})
 
 new Vue({
   router,
