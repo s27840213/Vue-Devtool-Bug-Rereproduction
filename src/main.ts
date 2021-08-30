3afdf78c2e3090bf1cb05bd7874c78db3c72c73d
giambihuang <nuphoto.giambi@gmail.com>
2021-08-30 08:35:57 +0800

    error tracking: apply tool only in production

--- src/main.ts
+++ src/main.ts
@@ -9,9 +9,6 @@ import { Store } from 'vuex'
 import { IEditorState } from './store/types'
 import { RecycleScroller } from 'vue-virtual-scroller'
 
-import * as Sentry from '@sentry/vue'
-import { Integrations } from '@sentry/tracing'
-
 Vue.config.productionTip = false
 Vue.use(VueRecyclerviewNew, vueColor)
 Vue.component('RecycleScroller', RecycleScroller)
@@ -19,27 +16,31 @@ const requireAll = (requireContext: __WebpackModuleApi.RequireContext) => requir
 const req = require.context('@/assets/icon', true, /\.svg$/)
 requireAll(req)
 
-Sentry.init({
-  Vue,
-  trackComponents: true,
-  maxBreadcrumbs: 10,
-  tracesSampleRate: 1.0,
-  environment: process.env.NODE_ENV,
-  dsn: process.env.VUE_APP_SENTRY_DSN,
-  release: process.env.VUE_APP_SENTRY_RELEASE,
-  integrations: [
-    new Integrations.BrowserTracing({
-      routingInstrumentation: Sentry.vueRouterInstrumentation(router)
-    })
-  ],
-  beforeBreadcrumb (breadcrumb, hint) {
-    if (hint && breadcrumb.category && ['xhr'].includes(breadcrumb.category)) {
-      const { __sentry_xhr__: request, response } = hint.xhr
-      Object.assign(breadcrumb.data, { response, requestBody: request.body })
+if (['production'].includes(process.env.NODE_ENV)) {
+  const Sentry = require('@sentry/vue')
+  const { Integrations } = require('@sentry/tracing')
+  Sentry.init({
+    Vue,
+    trackComponents: true,
+    maxBreadcrumbs: 10,
+    tracesSampleRate: 1.0,
+    environment: process.env.NODE_ENV,
+    dsn: process.env.VUE_APP_SENTRY_DSN,
+    release: process.env.VUE_APP_SENTRY_RELEASE,
+    integrations: [
+      new Integrations.BrowserTracing({
+        routingInstrumentation: Sentry.vueRouterInstrumentation(router)
+      })
+    ],
+    beforeBreadcrumb (breadcrumb: any, hint: any) {
+      if (hint && breadcrumb.category && ['xhr'].includes(breadcrumb.category)) {
+        const { __sentry_xhr__: request, response } = hint.xhr
+        Object.assign(breadcrumb.data, { response, requestBody: request.body })
+      }
+      return breadcrumb
     }
-    return breadcrumb
-  }
-})
+  })
+}
 
 new Vue({
   router,
