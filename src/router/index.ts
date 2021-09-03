5f51ab9f3d77ea1c036ec4f6f66c2b09517bf5a3
Gary <nuphoto.gary@gmail.com>
2021-09-03 12:32:11 +0800

    router: recover action when query with type and design_id

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,8 +3,8 @@ import VueRouter, { RouteConfig } from 'vue-router'
 import Editor from '../views/Editor.vue'
 import SignUp from '../views/Login/SignUp.vue'
 import Login from '../views/Login/Login.vue'
-// import store from '@/store'
-// import uploadUtils from '@/utils/uploadUtils'
+import store from '@/store'
+import uploadUtils from '@/utils/uploadUtils'
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
@@ -16,8 +16,8 @@ const routes: Array<RouteConfig> = [
     beforeEnter: async (to, from, next) => {
       try {
         next()
-        /*
         const urlParams = new URLSearchParams(window.location.search)
+        /*
         if (urlParams.has('token')) {
           const token = urlParams.get('token')
 
@@ -28,16 +28,16 @@ const routes: Array<RouteConfig> = [
             uploadUtils.uploadTmpJSON()
           }
         } else {
-          if (urlParams.has('type') && urlParams.has('design_id')) {
-            const type = urlParams.get('type')
-            const designId = urlParams.get('design_id')
+          */
+        if (urlParams.has('type') && urlParams.has('design_id')) {
+          const type = urlParams.get('type')
+          const designId = urlParams.get('design_id')
 
-            if (type && designId) {
-              uploadUtils.getDesign(type, designId)
-            }
+          if (type && designId) {
+            uploadUtils.getDesign(type, designId)
           }
         }
-        */
+        // }
       } catch (error) {
         console.log(error)
       }
