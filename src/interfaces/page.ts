b10a400d1dcf2abcdc34b7802297ea87f7934f6d
steve <nuphoto.steve@gmail.com>
2021-09-06 11:04:48 +0800

    resolve conflict bugs

--- src/interfaces/page.ts
+++ src/interfaces/page.ts
@@ -1,14 +1,11 @@
 import { IShape, IText, IImage, IGroup } from '@/interfaces/layer'
 
 export interface IBackgroundImage {
-<<<<<<< HEAD
   srcObj: {
     type: string,
     userId: string,
     assetId: string
   },
-=======
->>>>>>> feefa4a6375fad4c9d70795f9ba53927a75bdedb
   config: IImage,
   posX: number,
   posY: number
