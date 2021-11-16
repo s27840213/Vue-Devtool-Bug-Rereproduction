d5b864a0d4247b11636d6b63c23ab0ac88c00cee
TingAnYen <nuphoto.daniel@gmail.com>
2021-11-16 18:29:33 +0800

    [Feature] API with retries

--- src/apis/design.ts
+++ src/apis/design.ts
@@ -1,15 +1,35 @@
 import axios from '@/apis'
 import { AxiosPromise } from 'axios'
 
+const DEFAULT_RETRY_LIMIT = 3
+
 export default {
-  getDesigns: (token: string, path: string, folderOnly: boolean, sortByField: string, sortByDescending: boolean): AxiosPromise => axios('/list-asset', {
-    method: 'POST',
-    data: {
-      type: 'design',
-      token,
-      data: folderOnly ? 1 : 2,
-      order_by: `${sortByField}:${sortByDescending ? 'desc' : 'asc'}`,
-      path: path.replace('/', ',')
+  async requestWithRetry(api: () => AxiosPromise, retryLimit = DEFAULT_RETRY_LIMIT, retriedTimes = 0): Promise<any> {
+    try {
+      return await api()
+    } catch (error) {
+      console.log('design API failed:', error)
+      if (retriedTimes >= retryLimit) {
+        throw error
+      }
+      console.log(`retrying: ${retriedTimes + 1}/${retryLimit}`)
+      return await new Promise<any>((resolve) => {
+        setTimeout(() => {
+          resolve(this.requestWithRetry(api, retryLimit, retriedTimes + 1))
+        }, 1000)
+      })
     }
-  })
+  },
+  async getDesigns(token: string, path: string, folderOnly: boolean, sortByField: string, sortByDescending: boolean): Promise<any> {
+    return await this.requestWithRetry(() => axios('/list-asset', {
+      method: 'POST',
+      data: {
+        type: 'design',
+        token,
+        data: folderOnly ? 1 : 2,
+        order_by: `${sortByField}:${sortByDescending ? 'desc' : 'asc'}`,
+        path: path.replace('/', ',')
+      }
+    }))
+  }
 }
