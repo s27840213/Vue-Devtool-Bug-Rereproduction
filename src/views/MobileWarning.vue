c22aa90801067d2863a2384d78f70f57f1a6594e
TingAnYen <nuphoto.daniel@gmail.com>
2021-11-26 20:55:15 +0800

    [Enhancement] enhance landscape view layout

--- src/views/MobileWarning.vue
+++ src/views/MobileWarning.vue
@@ -2,8 +2,9 @@
   div(class="mobile-warning")
     div(class="mobile-warning__content")
       div(class="mobile-warning__logo")
-        svg-icon(iconName="logo"
-                iconWidth="100%")
+        div(class="mobile-warning__logo-wrapper")
+          svg-icon(iconName="logo"
+                  iconWidth="100%")
       img(class="mobile-warning__image"
           :src="require('@/assets/img/jpg/mobilewarning/mobile.jpg')")
       div(class="mobile-warning__title")
@@ -13,10 +14,11 @@
           span 請使用電腦版瀏覽器開啟 Vivipic 編輯器
         div(class="mobile-warning__description-line")
           span 以獲得最佳的設計體驗。
-      div(class="mobile-warning__button-wrapper")
-        div(class="mobile-warning__button"
-            @click="goToHome")
-          span 回到首頁
+      div(class="mobile-warning__button-outter")
+        div(class="mobile-warning__button-wrapper")
+          div(class="mobile-warning__button"
+              @click="goToHome")
+            span 回到首頁
 </template>
 
 <script lang="ts">
@@ -44,17 +46,22 @@ export default Vue.extend({
     flex-direction: column;
     align-items: center;
     @media (min-aspect-ratio: 1/1) {
-      width: 65vh;
+      width: 100vw;
     }
   }
   &__logo {
-    transform: scaleY(0.9);
-    transform-origin: bottom;
-    position: relative;
     width: 55%;
-    height: 0;
+    @media (min-aspect-ratio: 1/1) {
+      width: 40vh;
+    }
+  }
+  &__logo-wrapper {
+    position: relative;
+    width: 100%;
     padding-top: 23%;
     > svg {
+      transform: scaleY(0.9);
+      transform-origin: bottom;
       position: absolute;
       top: 0;
       left: 0;
@@ -64,10 +71,12 @@ export default Vue.extend({
     margin-top: min(4.5vw, 50px);
     width: 100%;
     height: auto;
+    @media (min-aspect-ratio: 1/1) {
+      width: 60vh;
+    }
   }
   &__title {
     margin-top: min(3vw, 25px);
-    height: 34px;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -80,6 +89,9 @@ export default Vue.extend({
       text-indent: 0.165em;
       color: black;
     }
+    @media (min-aspect-ratio: 1/1) {
+      margin-top: 3vh;
+    }
   }
   &__description {
     margin-top: min(2vw, 15px);
@@ -87,9 +99,11 @@ export default Vue.extend({
     flex-wrap: wrap;
     align-items: center;
     justify-content: center;
+    @media (min-aspect-ratio: 1/1) {
+      margin-top: 2vh;
+    }
   }
   &__description-line {
-    height: 26px;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -101,13 +115,18 @@ export default Vue.extend({
       color: black;
     }
   }
-  &__button-wrapper {
-    position: relative;
+  &__button-outter {
     margin-top: min(3vw, 25px);
     width: min(40%, 139px);
-    height: 0;
-    padding-top: min(max(7%, 30px), 39px);
     background-color: setColor(blue-1);
+    @media (min-aspect-ratio: 1/1) {
+      margin-top: 3vh;
+    }
+  }
+  &__button-wrapper {
+    position: relative;
+    width: 100%;
+    padding-top: min(max(7%, 30px), 39px);
   }
   &__button {
     position: absolute;
