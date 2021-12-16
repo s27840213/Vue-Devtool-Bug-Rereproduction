1f69a0bf34f9b7355609ae8fb1d62509a6a156ee
Gary <nuphoto.gary@gmail.com>
2021-12-16 13:56:51 +0800

    fix: fix wrong store function

--- src/router/handler.ts
+++ src/router/handler.ts
@@ -44,7 +44,7 @@ export async function editorRouteHandler(_to: Route, from: Route, next: Navigati
 
     // reset (close) page preview mode
     store.commit('page/SET_isShowPagePreview', false)
-    store.commit('page/SET_detailPageMode', false)
+    store.commit('page/SET_showPagePanel', false)
 
     if (url) {
       // e.g.: https://test.vivipic.com/editor?url=template.vivipic.com%2Fexport%2F9XBAb9yoKlJbzLiWNUVM%2F211123164456873giej3iKR%2Fpage_0.json%3Fver%3DJeQnhk9N%26token%3DVtOldDgVuwPIWP0Y%26team_id%3D9XBAb9yoKlJbzLiWNUVM
