cd17f642a739b1722ce2ec3ab4ad3bfab36e23ee
TingAnYen <nuphoto.daniel@gmail.com>
2022-01-04 15:10:39 +0800

    [Fix] change template center keyword for <locale>_show

--- src/views/TemplateCenter.vue
+++ src/views/TemplateCenter.vue
@@ -391,6 +391,7 @@ export default Vue.extend({
     },
     composeKeyword() {
       const res = [`locale::${this.$i18n.locale}`]
+      // const res = ['group::0']
       const tags = []
       let themes: string[] = []
       if (this.searchbarKeyword !== '') {
