f79c3a8f5e1daca639227c259d38d1f1f824360a
TingAnYen <nuphoto.daniel@gmail.com>
2022-01-05 10:37:55 +0800

    [Fix ] resizeUtils align NuController

--- src/utils/resizeUtils.ts
+++ src/utils/resizeUtils.ts
@@ -57,8 +57,8 @@ class ResizeUtils {
             imgX,
             imgY
           })
-          const clipPath = `M0,0h${width}v${height}h${-width}z`
-          frameUtils.updateFrameLayerProps(pageIndex, layerIndex, 0, { clipPath })
+          // const clipPath = `M0,0h${width}v${height}h${-width}z`
+          // frameUtils.updateFrameLayerProps(pageIndex, layerIndex, 0, { clipPath })
           scale = 1
         }
         break
