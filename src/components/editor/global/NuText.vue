6541bbedf47edea36e2a970b8f5ded33623fd7f1
TingAnYen <nuphoto.daniel@gmail.com>
2022-01-06 11:52:31 +0800

    [Fix] typo: config.styles.scale not config.scale

--- src/components/editor/global/NuText.vue
+++ src/components/editor/global/NuText.vue
@@ -75,7 +75,7 @@ export default Vue.extend({
       if (typeof this.subLayerIndex === 'undefined') {
         ControlUtils.updateLayerSize(this.pageIndex, this.layerIndex, textHW.width, textHW.height, this.getLayerScale)
       } else if (this.subLayerIndex === this.getLayer(this.pageIndex, this.layerIndex).layers.length - 1) {
-        console.log(textHW.width, textHW.height)
+        // console.log(textHW.width, textHW.height)
         const group = this.getLayer(this.pageIndex, this.layerIndex) as IGroup
         LayerUtils.updateSubLayerStyles(this.pageIndex, this.layerIndex, this.subLayerIndex, { width: textHW.width, height: textHW.height })
         const { width, height } = calcTmpProps(group.layers, group.styles.scale)
@@ -148,27 +148,27 @@ export default Vue.extend({
     autoResize(): {width: number, height: number} {
       if (this.$route.name !== 'Preview' || this.config.widthLimit === -1) return TextUtils.getTextHW(this.config, this.config.widthLimit)
       const dimension = this.config.styles.writingMode.includes('vertical') ? 'width' : 'height'
+      const scale = this.config.styles.scale
       let direction = 0
       let shouldContinue = true
       let widthLimit = this.config.widthLimit
       let autoSize = TextUtils.getTextHW(this.config, widthLimit)
       const originDimension = this.config.styles[dimension]
       while (shouldContinue) {
-        console.log(widthLimit, direction)
+        // console.log(widthLimit, direction)
         const autoDimension = autoSize[dimension]
-        console.log(autoDimension, originDimension, this.config.scale)
-        if (autoDimension - originDimension > 5 * this.config.scale) {
+        if (autoDimension - originDimension > 5 * scale) {
           if (direction < 0) break
           if (direction >= 20) return TextUtils.getTextHW(this.config, this.config.widthLimit)
-          widthLimit += this.config.scale
+          widthLimit += scale
           direction += 1
           autoSize = TextUtils.getTextHW(this.config, widthLimit)
           continue
         }
-        if (originDimension - autoDimension > 5 * this.config.scale) {
+        if (originDimension - autoDimension > 5 * scale) {
           if (direction > 0) break
           if (direction <= -20) return TextUtils.getTextHW(this.config, this.config.widthLimit)
-          widthLimit -= this.config.scale
+          widthLimit -= scale
           direction -= 1
           autoSize = TextUtils.getTextHW(this.config, widthLimit)
           continue
