7f43e9fb101843523d99b2d2f86f34d1007c16fa
mingchi <nuphoto.mingchi@gmail.com>
2022-01-11 11:34:33 +0800

    conditional pull request

--- bash/create_pull_request.sh
+++ bash/create_pull_request.sh
@@ -1,27 +1,87 @@
 #!/bin/bash
-echo "create pull request"
 apt-get update
 apt-get -y install curl jq
+[[ "$BITBUCKET_BRANCH" == "develop" || $BITBUCKET_BRANCH == bugfix/* ]] && export DESTINATION_BRANCH="develop" || export DESTINATION_BRANCH="master"
 export BITBUCKET_TRIGGERER_USERNAME=$(curl -X GET -g "https://api.bitbucket.org/2.0/users/${BITBUCKET_STEP_TRIGGERER_UUID}" | jq --raw-output '.display_name')
-export DEFAULT_REVIEWERS=$(curl https://api.bitbucket.org/2.0/repositories/${BITBUCKET_REPO_OWNER}/${BITBUCKET_REPO_SLUG}/default-reviewers \
-    -s -S -f -X GET \
-    -H "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" | jq '.values' | jq 'map({uuid})' |  jq '[.[] | select(.uuid!="{'"${BITBUCKET_STEP_TRIGGERER_UUID}"'}")]')
-curl --request POST \
-    --url "https://api.bitbucket.org/2.0/repositories/mingchi/nueditor/pullrequests" \
-    --header "Accept: application/json" \
-    --header "Content-Type: application/json" \
-    --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
-    --data '{
-      "title": "Pull request to develop by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
-      "source": {
-        "branch": {
-          "name": "'"${BITBUCKET_BRANCH}"'"
-        }
-      },
-      "destination": {
-        "branch": {
-          "name": "develop"
-        }
-      },
-      "reviewers": '"${DEFAULT_REVIEWERS}"'
-    }'
+if [[ "$BITBUCKET_BRANCH" == "develop" ]]
+then
+    echo "create pull request to master"
+    curl --request POST \
+        --url "https://api.bitbucket.org/2.0/repositories/mingchi/nueditor/pullrequests" \
+        --header "Accept: application/json" \
+        --header "Content-Type: application/json" \
+        --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
+        --data '{
+          "title": "Pull request to develop by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
+          "source": {
+            "branch": {
+              "name": "'"${BITBUCKET_BRANCH}"'"
+            }
+          },
+          "destination": {
+            "branch": {
+              "name": "master"
+            }
+          }
+        }'
+elif [[ "$BITBUCKET_BRANCH" == hotfix* ]]
+then
+    echo "create pull request to master and develop"
+    curl --request POST \
+        --url "https://api.bitbucket.org/2.0/repositories/mingchi/nueditor/pullrequests" \
+        --header "Accept: application/json" \
+        --header "Content-Type: application/json" \
+        --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
+        --data '{
+          "title": "Pull request to develop by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
+          "source": {
+            "branch": {
+              "name": "'"${BITBUCKET_BRANCH}"'"
+            }
+          },
+          "destination": {
+            "branch": {
+              "name": "master"
+            }
+          }
+        }'
+    curl --request POST \
+        --url "https://api.bitbucket.org/2.0/repositories/mingchi/nueditor/pullrequests" \
+        --header "Accept: application/json" \
+        --header "Content-Type: application/json" \
+        --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
+        --data '{
+          "title": "Pull request to develop by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
+          "source": {
+            "branch": {
+              "name": "'"${BITBUCKET_BRANCH}"'"
+            }
+          },
+          "destination": {
+            "branch": {
+              "name": "develop"
+            }
+          }
+        }'
+else
+    echo "create pull request to master"
+    curl --request POST \
+        --url "https://api.bitbucket.org/2.0/repositories/mingchi/nueditor/pullrequests" \
+        --header "Accept: application/json" \
+        --header "Content-Type: application/json" \
+        --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
+        --data '{
+          "title": "Pull request to develop by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
+          "source": {
+            "branch": {
+              "name": "'"${BITBUCKET_BRANCH}"'"
+            }
+          },
+          "destination": {
+            "branch": {
+              "name": "master"
+            }
+          }
+        }'
+fi
+
