c46355a26f98cb954d22e2e6942dfaf441d7f0e0
TingAnYen <nuphoto.daniel@gmail.com>
2022-01-19 11:36:03 +0800

    [Fix] writingMode change should consider widthLimit

--- src/utils/textPropUtils.ts
+++ src/utils/textPropUtils.ts
@@ -124,7 +124,7 @@ class TextPropUtils {
         const config = (typeof tmpLayerIndex === 'undefined' ? this.getCurrLayer : this.getCurrLayer.layers[tmpLayerIndex]) as IText
         const writingMode = config.styles.writingMode.includes('vertical') ? 'vertical-lr' : 'initial'
         if (typeof tmpLayerIndex === 'undefined' && writingMode !== targetWritingMode) {
-          const { width, height } = TextUtils.getTextHW(config)
+          const { width, height } = TextUtils.getTextHW(config, config.widthLimit)
           LayerUtils.updateLayerStyles(this.pageIndex, this.layerIndex, { width: height, height: width })
           // @TODO: need to reallocate position of each layer
         }
