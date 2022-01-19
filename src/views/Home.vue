a43ee73b8c527009679f2a3791044c11b38a2c37
Gary <nuphoto.gary@gmail.com>
2022-01-19 10:15:43 +0800

    update: add cache to listall = 1 case and set token to fixed value

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -269,15 +269,16 @@ export default Vue.extend({
     }
     let keyword = this.tagString.replace(/,/gi, ' ')
     this.tags = this.tagString.split(',')
-    const tagTemplate = await this.getTagContent({ keyword, theme })
+    const cache = true
+    const tagTemplate = await this.getTagContent({ keyword, theme, cache })
     this.tagTemplateList = tagTemplate.data.content[0].list
 
     keyword = 'group::0;;order_by::popular'
-    const popularTemplate = await this.getTagContent({ keyword, theme })
+    const popularTemplate = await this.getTagContent({ keyword, theme, cache })
     this.popularTemplateList = popularTemplate.data.content[0].list
 
     keyword = 'group::0;;order_by::time'
-    const latestTemplate = await this.getTagContent({ keyword, theme })
+    const latestTemplate = await this.getTagContent({ keyword, theme, cache })
     this.latestTemplateList = latestTemplate.data.content[0].list
   },
   destroyed() {
