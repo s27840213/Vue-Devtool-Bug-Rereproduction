efb84b949bfae343e55b2554f8134e2a179a2a71
Gary <nuphoto.gary@gmail.com>
2022-01-22 02:10:53 +0800

    feat: add feature assets and update feature1 style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -36,7 +36,6 @@
                 @search="handleSearch")
             template(v-else)
               div(class="home-content__top-btn"
-                :type="'primary-lg'"
                 @click="newDesign()") {{$t('NN0274')}}
               img(class="home-content__top-img"
                 :src="require('@/assets/img/svg/homepage/header_img_us.png')")
@@ -66,6 +65,18 @@
             @click="goToTemplateCenterTheme(list.theme)") {{$t('NN0082')}}
         div(class="home-content__template")
           template-list(:theme="list.theme" type='template')
+      template(v-if="!isLogin")
+        div(class="home-content__feature-bg bg-f1")
+          div(class="home-content__feature-f1")
+            i18n(path="NN0372"
+             class="home-content__feature-title" tag="span")
+                template(#newline)
+                  br
+            div(class="home-content__feature-subtitle") {{$t('NN0373')}}
+            div(class="home-content__feature-btn"
+              @click="newDesign()") {{$t('NN0374')}}
+          img(class="home-content__feature-img"
+            :src="require('@/assets/img/svg/homepage/Feature 1_us.svg')")
       nu-footer(class="mt-50")
       div(v-if="showSizePopup"
         class="home__size")
@@ -298,11 +309,6 @@ export default Vue.extend({
     &-bg {
       width: 100%;
       object-fit: cover;
-      // background-size: contain;
-      // background-image: url("~@/assets/img/svg/homepage/top-bg.svg");
-      @include layout-mobile {
-        // background-image: url("~@/assets/img/svg/homepage/top-bg.svg");
-      }
     }
     &-white {
       position: absolute;
@@ -349,11 +355,7 @@ export default Vue.extend({
       transform: scale(0.9);
     }
     &-btn {
-      display: flex;
-      align-items: center;
-      justify-content: center;
       width: 240px;
-      height: 56px;
       margin-top: 20px;
       @include button-LG;
       @media screen and (max-width: 1440px) {
@@ -429,22 +431,62 @@ export default Vue.extend({
       }
     }
   }
-}
-.home-btn {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  cursor: pointer;
-  width: 200px;
-  height: 40px;
-  font-size: 20px;
-  font-weight: 700;
-  color: setColor(white);
-  background: setColor(blue-1);
-  @media screen and (max-width: 990px) {
-    width: 160px;
-    height: 35px;
-    font-size: 16px;
+  &__feature {
+    &-bg {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: #F3F6FA;
+      margin-top: 24px;
+      padding: 0 5%;
+      &.bg-f1 {
+        @media screen and (max-width: 768px) {
+          flex-direction: column-reverse;
+          align-items: flex-start;
+          padding: 0;
+        }
+      }
+    }
+    &-f1 {
+      display: flex;
+      flex-direction: column;
+      padding: 150px 0;
+      @media screen and (max-width: 768px) {
+        padding: 50px 24px 75px 24px;
+      }
+    }
+    &-title {
+      text-align: left;
+      @include text-H2;
+      width: 400px;
+      @media screen and (max-width: 540px) {
+        @include text-H5;
+        width: unset;
+      }
+    }
+    &-subtitle {
+      text-align: left;
+      color: setColor(gray-2);
+      padding-top: 20px;
+      @include body-LG;
+      @media screen and (max-width: 768px) {
+        @include body-SM;
+      }
+    }
+    &-btn {
+      width: 200px;
+      margin-top: 20px;
+      @include button-LG;
+    }
+    &-img {
+      width: 50%;
+      max-width: 640px;
+      @media screen and (max-width: 768px) {
+        margin:0 auto;
+        width: 95%;
+        max-width: unset;
+      }
+    }
   }
 }
 </style>
