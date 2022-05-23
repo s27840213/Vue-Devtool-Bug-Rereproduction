26da5f369b33d0ea0e3a3f8851009d88db4dc7cd
steve <nuphoto.steve@gmail.com>
2022-05-23 11:51:17 +0800

    update: image add missing design handle

--- src/apis/errorHandle.ts
+++ src/apis/errorHandle.ts
@@ -1,18 +1,19 @@
+/* eslint-disable camelcase */
 import axios from '@/apis'
 import authToken from './auth-token'
 import { AxiosPromise } from 'axios'
 import { IUploadMissingDesign } from '@/interfaces/api'
 class ErrorHandle {
   addMissingDesign (params: IUploadMissingDesign): AxiosPromise {
-    const { type, design_id } = params
+    const { token = '', type, design_id } = params
     return axios.request<void>({
       url: '/add-missing-design',
       method: 'POST',
       data: {
-        token: authToken().token || '',
+        token: authToken().token || token,
         type,
         design_id
-      }
+      } as IUploadMissingDesign
     })
   }
 }
