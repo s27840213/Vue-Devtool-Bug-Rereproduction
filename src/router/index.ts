16c70d9565021b07c29edcdfa608e244c8dbc145
Alan <nuphoto.alan@gmail.com>
2022-06-17 11:03:26 +0800

    fix: remove debug settings

--- src/router/index.ts
+++ src/router/index.ts
@@ -303,10 +303,10 @@ router.beforeEach(async (to, from, next) => {
     /**
      * @MobileDebug - comment the following two line, and use const json = appJSON, or the request will be blocked by CORS
      */
-    // const response = await fetch(`https://template.vivipic.com/static/app.json?ver=${generalUtils.generateRandomString(6)}`)
-    // const json = await response.json()
+    const response = await fetch(`https://template.vivipic.com/static/app.json?ver=${generalUtils.generateRandomString(6)}`)
+    const json = await response.json()
 
-    const json = appJson
+    // const json = appJson
 
     process.env.NODE_ENV === 'development' && console.log('static json loaded: ', json)
 
