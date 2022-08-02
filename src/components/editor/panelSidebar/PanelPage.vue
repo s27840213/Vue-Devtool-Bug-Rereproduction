ac5d6f68e9e6d09f2d9254bb56b2e5ce62887f1f
TingAnYen <nuphoto.daniel@gmail.com>
2022-08-02 15:44:28 +0800

    [Fix] don't use deepCopy since the update may be missed

--- src/components/editor/panelSidebar/PanelPage.vue
+++ src/components/editor/panelSidebar/PanelPage.vue
@@ -8,11 +8,11 @@
         style="padding: 5px 0;"
         @click.native="addPage(middlemostPageIndex+1)") {{$t('NN0139')}}
     div(class="panel-page-items")
-      template(v-for="(page, idx) in pages")
+      template(v-for="(page, idx) in getPages")
         div(class="panel-page__plus")
           panel-page-plus(:index="idx" last=false
             :class="{'pt-10': idx === 0}")
-        page-preview-page-wrapper(:index="idx" :pagename="page.name" type="panel" :config="page")
+        page-preview-page-wrapper(:index="idx" :pagename="page.name" type="panel" :config="wrappedPage(page)")
         div(v-if="idx+1 === getPageCount"
           class="panel-page__plus")
           panel-page-plus(:index="idx+1" last=false)
@@ -24,7 +24,6 @@ import { mapGetters, mapMutations } from 'vuex'
 import PagePreviewPageWrapper from '@/components/editor/pagePreview/PagePreviewPageWrapper.vue'
 import PanelPagePlus from '@/components/editor/pagePreview/PanelPagePlus.vue'
 import pageUtils from '@/utils/pageUtils'
-import generalUtils from '@/utils/generalUtils'
 import { IPage } from '@/interfaces/page'
 
 export default Vue.extend({
@@ -38,12 +37,7 @@ export default Vue.extend({
       middlemostPageIndex: 'getMiddlemostPageIndex'
     }),
     getPageCount(): number {
-      return this.pages.length
-    },
-    pages(): IPage[] {
-      const pages = generalUtils.deepCopy(this.getPages)
-      pageUtils.setAutoResizeNeededForPages(pages, false)
-      return pages
+      return this.getPages.length
     }
   },
   methods: {
@@ -57,6 +51,9 @@ export default Vue.extend({
         pos: position
       })
       this._setmiddlemostPageIndex(position)
+    },
+    wrappedPage(page: IPage) {
+      return { ...page, isAutoResizeNeeded: false }
     }
   }
 })
