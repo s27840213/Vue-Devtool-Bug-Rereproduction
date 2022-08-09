11880737d5019e2398183bd52655b79e114c5ed7
TingAnYen <nuphoto.daniel@gmail.com>
2022-08-09 17:55:28 +0800

    [Init] setup vivisticker router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,41 +1,14 @@
 import Vue from 'vue'
 import VueRouter, { RawLocation, Route, RouteConfig } from 'vue-router'
-import Editor from '../views/Editor.vue'
-import SignUp from '../views/Login/SignUp.vue'
-import Login from '../views/Login/Login.vue'
-import MyDesign from '../views/MyDesign.vue'
-import Home from '../views/Home.vue'
-import Settings from '../views/Settings.vue'
-import TemplateCenter from '../views/TemplateCenter.vue'
-import MobileWarning from '../views/MobileWarning.vue'
-import Preview from '../views/Preview.vue'
+import ViviSticker from '../views/ViviSticker.vue'
 import SvgIconView from '../views/SvgIconView.vue'
-import BrandKit from '../views/BrandKit.vue'
-import Pricing from '../views/Pricing.vue'
 import store from '@/store'
-import { editorRouteHandler } from './handler'
 import i18n from '@/i18n'
 import localeUtils from '@/utils/localeUtils'
 import logUtils from '@/utils/logUtils'
-import assetUtils from '@/utils/assetUtils'
-import brandkitUtils from '@/utils/brandkitUtils'
-import appJson from '@/assets/json/app.json'
 import generalUtils from '@/utils/generalUtils'
 
 Vue.use(VueRouter)
-
-const MOBILE_ROUTES = [
-  'Home',
-  'TemplateCenter',
-  'Settings',
-  'SignUp',
-  'Login',
-  'MobileWarning',
-  'Preview',
-  'MobileEditor',
-  'MyDesign',
-  'Pricing'
-]
 
 // Ingore some normal router console error
 const originalPush = VueRouter.prototype.push
@@ -69,8 +42,8 @@ VueRouter.prototype.replace = function repalce(location: RawLocation): Promise<R
 const routes: Array<RouteConfig> = [
   {
     path: '',
-    name: 'Home',
-    component: Home,
+    name: 'ViviSticker',
+    component: ViviSticker,
     beforeEnter: async (to, from, next) => {
       try {
         next()
@@ -78,132 +51,6 @@ const routes: Array<RouteConfig> = [
         console.log(error)
       }
     }
-  },
-  {
-    path: 'editor',
-    name: 'Editor',
-    component: Editor,
-    beforeEnter: editorRouteHandler
-  },
-  // {
-  //   path: 'mobile-editor',
-  //   name: 'MobileEditor',
-  //   component: MobileEditor,
-  //   // eslint-disable-next-line space-before-function-paren
-  //   beforeEnter: editorRouteHandler
-  // },
-  {
-    path: 'preview',
-    name: 'Preview',
-    component: Preview,
-    beforeEnter: async (to, from, next) => {
-      try {
-        const urlParams = new URLSearchParams(window.location.search)
-        const url = urlParams.get('url')
-
-        if (url) {
-          // e.g.: /preview?url=template.vivipic.com%2Fexport%2F<design_team_id>%2F<design_export_id>%2Fpage_<page_index>.json%3Fver%3DJeQnhk9N%26token%3DQT0z7B3D3ZuXVp6R%26team_id%3DPUPPET
-          const hasToken = url.indexOf('token=') !== -1
-          let tokenKey = ''
-          let src = url
-          if (hasToken) {
-            tokenKey = url.match('&token') ? '&token=' : '?token='
-            src = url.substring(0, hasToken ? url.indexOf(tokenKey) : undefined)
-            const token = url.substring((src + tokenKey).length, url.indexOf('&team_id='))
-            const teamId = url.substr((src + tokenKey + token + '&team_id=').length)
-            store.commit('user/SET_STATE', { token, teamId })
-          }
-          const response = await (await fetch(`https://${src}`)).json()
-          await assetUtils.addTemplate(response, { pageIndex: 0 })
-          store.commit('file/SET_setLayersDone')
-          store.commit('user/SET_STATE', { userId: 'backendRendering' })
-        }
-        next()
-      } catch (error) {
-        console.log(error)
-      }
-    }
-  },
-  {
-    path: 'signup',
-    name: 'SignUp',
-    props: route => ({ redirect: route.query.redirect }),
-    component: SignUp,
-    beforeEnter: async (to, from, next) => {
-      try {
-        if (store.getters['user/isLogin']) {
-          next({ path: from.query.redirect as string || '/' })
-        } else {
-          next()
-        }
-      } catch (error) {
-        console.log(error)
-      }
-    }
-  },
-  {
-    path: 'login',
-    name: 'Login',
-    props: route => ({ redirect: route.query.redirect }),
-    component: Login,
-    beforeEnter: async (to, from, next) => {
-      try {
-        if (to.query.type) {
-          next()
-        } else {
-          if (store.getters['user/isLogin']) {
-            next({ path: from.query.redirect as string || '/' })
-          } else {
-            next()
-          }
-        }
-      } catch (error) {
-        console.log(error)
-      }
-    }
-  },
-  {
-    path: 'mydesign/:view?',
-    name: 'MyDesign',
-    component: MyDesign,
-    props: true
-  },
-  {
-    path: 'templates',
-    name: 'TemplateCenter',
-    component: TemplateCenter
-  },
-  {
-    path: 'settings/:view?',
-    name: 'Settings',
-    component: Settings,
-    props: true
-  },
-  {
-    path: 'mobilewarning',
-    name: 'MobileWarning',
-    component: MobileWarning
-  },
-  {
-    path: 'brandkit',
-    name: 'BrandKit',
-    component: BrandKit,
-    beforeEnter: async (to, from, next) => {
-      try {
-        if (!brandkitUtils.isBrandkitAvailable) {
-          next({ path: '/' })
-        } else {
-          next()
-        }
-      } catch (error) {
-        console.log(error)
-      }
-    }
-  },
-  {
-    path: 'pricing',
-    name: 'Pricing',
-    component: Pricing
   }
 ]
 
@@ -267,26 +114,6 @@ router.beforeEach(async (to, from, next) => {
     localStorage.setItem('campaign', campaign)
   }
 
-  // Force login in these page
-  if (['Settings', 'MyDesign', 'BrandKit', 'Editor'].includes(to.name as string)) {
-    if (!store.getters['user/isLogin']) {
-      const token = localStorage.getItem('token')
-      if (token === '' || !token) {
-        next({ name: 'SignUp', query: { redirect: to.fullPath } })
-        return
-      } else {
-        await store.dispatch('user/login', { token: token })
-      }
-    }
-  } else {
-    if (!store.getters['user/isLogin']) {
-      const token = localStorage.getItem('token')
-      if (token && token.length > 0) {
-        await store.dispatch('user/login', { token: token })
-      }
-    }
-  }
-
   if (store.getters['user/getImgSizeMap'].length === 0 && (window as any).__PRERENDER_INJECTED === undefined) {
     /**
      * @MobileDebug - comment the following two line, and use const json = appJSON, or the request will be blocked by CORS
@@ -319,37 +146,6 @@ router.beforeEach(async (to, from, next) => {
         }
         store.commit('text/UPDATE_DEFAULT_FONT', { font })
       })
-  }
-  if (!MOBILE_ROUTES.includes(to.name ?? '') && (to.name === 'Editor' || !localStorage.getItem('not-mobile'))) {
-    let isMobile = false
-    const userAgent = navigator.userAgent || navigator.vendor
-    logUtils.setLog(`Read device width: ${window.screen.width}`)
-    logUtils.setLog(`User agent: ${userAgent}`)
-    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(userAgent)) {
-      // is Desktop
-      if (userAgent.indexOf('Mac OS X') > 0) {
-        // is Mac (could be iPad)
-        if (window.screen.width <= 1024) {
-          // less than iPad Pro width
-          isMobile = true
-        } // wider
-      }
-      // not Mac
-    } else {
-      // is Mobile
-      isMobile = true
-    }
-    if (isMobile) {
-      logUtils.setLog('=> as mobile')
-      if (to.name !== 'Editor' || !localStorage.getItem('not-mobile')) {
-        next({ name: 'MobileWarning', query: { width: window.screen.width.toString(), url: to.fullPath } })
-        return
-      } else {
-        store.commit('SET_useMobileEditor', true)
-      }
-    } else {
-      logUtils.setLog('=> as non-mobile')
-    }
   }
 
   next()
---
+++ src/views/ViviSticker.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  div(class="vivisticker")
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  components: {
+  },
+  data() {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.panel-brand {
+  @include size(100%, 100%);
+  overflow: hidden;
+}
+
+.dim-background {
+  @include size(100%, 100%);
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.3);
+  z-index: 1000;
+}
+</style>
