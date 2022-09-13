c1e67edb43992d31c649de7ebcd9a7877d3f8c27
TingAnYen <nuphoto.daniel@gmail.com>
2022-09-13 14:25:15 +0800

    [Fix] remove longpress on controller for text cursor

--- src/components/editor/global/NuController.vue
+++ src/components/editor/global/NuController.vue
@@ -20,7 +20,6 @@
         @pointerdown="moveStart"
         @mouseenter="toggleHighlighter(pageIndex,layerIndex, true)"
         @mouseleave="toggleHighlighter(pageIndex,layerIndex, false)"
-        v-press="isTouchDevice? onPress : -1"
         @dblclick="onDblClick")
       //- template(v-if="((['group', 'tmp', 'frame'].includes(getLayerType))) && !isTouchDevice")
       template(v-if="((['group', 'tmp', 'frame'].includes(getLayerType))) && !isDragging")
