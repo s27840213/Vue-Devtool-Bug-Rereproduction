1259b06e73d3b063bb891244974fa9eeea0e5273
Nathan <nuphoto.nathan@gmail.com>
2022-09-30 13:50:13 +0800

    [Fix] Adjust panel text item size.

--- src/components/category/CategoryTextItem.vue
+++ src/components/category/CategoryTextItem.vue
@@ -36,8 +36,8 @@ export default Vue.extend({
     itemStyle(): any {
       const { width } = this.item.preview || {
         width: generalUtils.isTouchDevice()
-          ? (window.innerWidth - 54) / 3
-          : 145
+          ? (window.innerWidth - 54) / 3 - 10 // ([window width] - [padding and gap]) / 3 - [item margin]
+          : 135
       }
       return {
         objectFit: 'contain',
