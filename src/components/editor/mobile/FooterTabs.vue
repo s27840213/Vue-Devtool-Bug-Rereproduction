84bc272c2c8313c3465c7ef373bc7594ef21815f
Alan <nuphoto.alan@gmail.com>
2022-10-26 11:00:18 +0800

    feat: a/b-test for footer tabs

--- src/components/editor/mobile/FooterTabs.vue
+++ src/components/editor/mobile/FooterTabs.vue
@@ -9,8 +9,8 @@
           svg-icon(class="mb-5 click-disabled"
             :iconName="tab.icon"
             :iconColor="(tab.disabled || isLocked) ? 'gray-2' : currTab ===  tab.panelType ? 'blue-1' :'white'"
-            :iconWidth="'24px'")
-          span(class="text-body-4 no-wrap click-disabled"
+            :iconWidth="'20px'")
+          span(class="body-4 no-wrap click-disabled"
           :class="(tab.disabled || isLocked) ? 'text-gray-2' :(currTab ===  tab.panelType ) ? 'text-blue-1' : 'text-white'") {{tab.text}}
 </template>
 <script lang="ts">
@@ -138,7 +138,7 @@ export default Vue.extend({
       return [
         { icon: 'edit', text: `${this.$t('NN0504')}`, hidden: this.selectMultiple || this.hasSubSelectedLayer || this.isGroup },
         { icon: 'font', text: generalUtils.capitalize(`${this.$tc('NN0353', 2)}`), panelType: 'fonts' },
-        { icon: 'font-size', text: `${this.$t('NN0492')}`, panelType: 'font-size' },
+        { icon: 'font-size', text: `${this.$t('NN0122')}`, panelType: 'font-size' },
         {
           icon: 'color',
           text: `${this.$t('NN0495')}`,
@@ -148,7 +148,7 @@ export default Vue.extend({
           }
         },
         { icon: 'effect', text: `${this.$t('NN0491')}`, panelType: 'text-effect' },
-        { icon: 'spacing', text: `${this.$t('NN0109')}`, panelType: 'font-spacing' },
+        { icon: 'spacing', text: `${this.$t('NN0755')}`, panelType: 'font-spacing' },
         { icon: 'text-format', text: `${this.$t('NN0498')}`, panelType: 'font-format' }
         // { icon: 'copy-style', text: `${this.$t('NN0035')}`, panelType: 'text',hidden: true }
       ]
@@ -611,8 +611,8 @@ export default Vue.extend({
     display: grid;
     grid-template-rows: auto;
     grid-auto-flow: column;
-    grid-auto-columns: 65px;
-    column-gap: 32px;
+    grid-auto-columns: 50px;
+    column-gap: 16px;
     background-color: setColor(nav);
     padding: 8px 12px;
     @include no-scrollbar;
@@ -627,7 +627,7 @@ export default Vue.extend({
     box-sizing: border-box;
     padding: 0px 4px;
     > span {
-      transform: scale(0.8);
+      transform: scale(0.83);
       transition: background-color 0.2s, color 0.2s;
     }
   }
