f8c7159e810462d1697fbd56a96b993df5f8b48e
Alan <nuphoto.alan@gmail.com>
2022-10-26 11:43:28 +0800

    fix: a/b test for the footer tabs

--- src/components/editor/mobile/FooterTabs.vue
+++ src/components/editor/mobile/FooterTabs.vue
@@ -611,7 +611,7 @@ export default Vue.extend({
     display: grid;
     grid-template-rows: auto;
     grid-auto-flow: column;
-    grid-auto-columns: min-content;
+    grid-auto-columns: 70px;
     column-gap: 20px;
     background-color: setColor(nav);
     padding: 8px 12px;
