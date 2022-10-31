6fbe21fea387405f361085d47698746937984774
Nathan <nuphoto.nathan@gmail.com>
2022-10-31 10:40:14 +0800

    [Add] Apply hide-scrollbar on EditorView.

--- src/components/editor/EditorView.vue
+++ src/components/editor/EditorView.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-  div(class="editor-view scrollbar-gray-thin"
+  div(class="editor-view"
       :class="isBackgroundImageControl ? 'dim-background' : 'bg-gray-5'"
       :style="brushCursorStyles()"
       @pointerdown="!inBgRemoveMode ? !getInInGestureMode ? selectStart($event) : dragEditorViewStart($event) : null"
@@ -582,7 +582,26 @@ export default Vue.extend({
 $REULER_SIZE: 20px;
 
 .editor-view {
-  overflow: scroll;
+  @include hide-scrollbar;
+  overflow: auto; // Firefox will use this
+  overflow: overlay;
+  @include firefoxOnly {
+    scrollbar-width: thin;
+    scrollbar-color: transparent transparent;
+    &:hover {
+      scrollbar-color: setColor(gray-3) transparent;
+    }
+  }
+  &::-webkit-scrollbar {
+    width: 8px;
+  }
+  &::-webkit-scrollbar-thumb {
+    border: unset;
+    background-color: setColor(gray-3);;
+  }
+  &::-webkit-scrollbar-corner { // Coner of two scrollbars.
+    background-color: transparent;
+  }
   position: relative;
   z-index: setZindex("editor-view");
 
