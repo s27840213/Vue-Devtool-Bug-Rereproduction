124ee21d68137a5e575abef32d55951a9f3a8c5c
ZhengYan <nuphoto.zhengyan@gmail.com>
2022-11-09 15:51:26 +0800

    fix: fix styles

--- src/components/mydesign/PageSizeSelector.vue
+++ src/components/mydesign/PageSizeSelector.vue
@@ -1,36 +1,33 @@
 <template lang="pug">
   div(class="page-size-selector")
-    div(class="page-size-selector__body-row first-row")
-      span(class="page-size-selector__body__title subtitle-2 text-black") {{$t('NN0023')}}
-    div(class="page-size-selector__body-row")
-      div(class="page-size-selector__body__custom")
-        property-bar(class="page-size-selector__body__custom__box"
-                    :class="(selectedFormatKey === 'custom' ? 'border-black-1' : `border-${isDarkTheme ? 'white' : 'gray-2'}`) + (isValidate ? widthValid ? '' : ' input-invalid' : '')")
-          input(class="body-3" type="number" min="0" ref="inputWidth"
-                :class="(selectedFormatKey === 'custom' ? 'text-black' : defaultTextColor)"
-                :value="pageWidth || null" :placeholder="isMobile ? $t('NN0320') : $t('NN0163', {term: $t('NN0320')})" @click="selectFormat('custom')" @input="setPageWidth")
-          span(class="body-4"
-              :class="selectedFormatKey === 'custom' ? 'text-black' : defaultTextColor") W
-        svg-icon(class="pointer"
-            :iconName="isLocked ? 'lock' : 'unlock'"
-            iconWidth="15px" :iconColor="!isLockDisabled ? (selectedFormatKey === 'custom' ? 'black' : (isDarkTheme ? 'white' : 'gray-3')) : 'gray-3'"
-            @click.native="toggleLock()")
-        property-bar(class="page-size-selector__body__custom__box"
-                    :class="(selectedFormatKey === 'custom' ? 'border-black-1' : `border-${isDarkTheme ? 'white' : 'gray-2'}`) + (isValidate ? heightValid ? '' : ' input-invalid' : '')")
-          input(class="body-3" type="number" min="0"
-                :class="selectedFormatKey === 'custom' ? 'text-black' : defaultTextColor"
-                :value="pageHeight || null" :placeholder="isMobile ? $t('NN0319') : $t('NN0163', {term: $t('NN0319')})" @click="selectFormat('custom')" @input="setPageHeight")
-          span(class="body-4"
-              :class="selectedFormatKey === 'custom' ? 'text-black' : defaultTextColor") H
-        div(v-if="isValidate && !isCustomValid"
-          class="page-size-selector__body__custom__err text-red") {{errorMsg}}
+    div(class="page-size-selector__body__custom")
+      property-bar(class="page-size-selector__body__custom__box"
+                  :class="(selectedFormatKey === 'custom' ? 'border-black-1' : `border-${isDarkTheme ? 'white' : 'gray-2'}`) + (isValidate ? widthValid ? '' : ' input-invalid' : '')")
+        input(class="body-3" type="number" min="0" ref="inputWidth"
+              :class="(selectedFormatKey === 'custom' ? 'text-black' : defaultTextColor)"
+              :value="pageWidth || null" :placeholder="isMobile ? $t('NN0320') : $t('NN0163', {term: $t('NN0320')})" @click="selectFormat('custom')" @input="setPageWidth")
+        span(class="body-4 page-size-selector__body__custom__box__input-label"
+            :class="selectedFormatKey === 'custom' ? 'text-black' : defaultTextColor") W
+      svg-icon(class="pointer"
+          :iconName="isLocked ? 'lock' : 'unlock'"
+          iconWidth="20px" :iconColor="!isLockDisabled ? (selectedFormatKey === 'custom' ? 'black' : (isDarkTheme ? 'white' : 'gray-3')) : 'gray-3'"
+          @click.native="toggleLock()")
+      property-bar(class="page-size-selector__body__custom__box"
+                  :class="(selectedFormatKey === 'custom' ? 'border-black-1' : `border-${isDarkTheme ? 'white' : 'gray-2'}`) + (isValidate ? heightValid ? '' : ' input-invalid' : '')")
+        input(class="body-3" type="number" min="0"
+              :class="selectedFormatKey === 'custom' ? 'text-black' : defaultTextColor"
+              :value="pageHeight || null" :placeholder="isMobile ? $t('NN0319') : $t('NN0163', {term: $t('NN0319')})" @click="selectFormat('custom')" @input="setPageHeight")
+        span(class="body-4 page-size-selector__body__custom__box__input-label"
+            :class="selectedFormatKey === 'custom' ? 'text-black' : defaultTextColor") H
+      div(v-if="isValidate && !isCustomValid"
+        class="page-size-selector__body__custom__err text-red") {{errorMsg}}
     div(class="page-size-selector__body__hr horizontal-rule bg-gray-4")
     div(class="page-size-selector__container")
         div(class="page-size-selector__body-row first-row")
           span(class="page-size-selector__body__title subtitle-2 text-black") {{$t('NN0024')}}
         div(v-if="!isLayoutReady" class="page-size-selector__body-row-center")
           svg-icon(iconName="loading" iconWidth="25px" iconHeight="10px" :iconColor="defaultTextColor")
-        div(v-for="(format, index) in recentlyUsed" class="page-size-selector__body-row pointer"
+        div(v-for="(format, index) in recentlyUsed" class="page-size-selector__body-row item pointer"
             @click="selectFormat(`recent-${index}`)")
           radio-btn(class="page-size-selector__body__radio"
                     :isSelected="selectedFormatKey === `recent-${index}`",
@@ -39,19 +36,18 @@
                     @select="selectFormat")
           span(class="page-size-selector__body__recently body-3 pointer"
                 :class="selectedFormatKey === `recent-${index}` ? 'text-black' : defaultTextColor") {{ makeFormatString(format) }}
-        div(class="mt-10")
         div(class="page-size-selector__body-row first-row")
           span(class="page-size-selector__body__title subtitle-2 text-black") {{$t('NN0025')}}
         div(v-if="!isLayoutReady" class="page-size-selector__body-row-center")
           svg-icon(iconName="loading" iconWidth="25px" iconHeight="10px" iconColor="white")
-        div(v-for="(format, index) in formatList" class="page-size-selector__body-row pointer"
+        div(v-for="(format, index) in formatList" class="page-size-selector__body-row item pointer"
             @click="selectFormat(`preset-${index}`)")
           radio-btn(class="page-size-selector__body__radio"
                     :isSelected="selectedFormatKey === `preset-${index}`",
                     :circleColor="isDarkTheme ? 'white' : 'gray-2'"
                     :formatKey="`preset-${index}`",
                     @select="selectFormat")
-          span(class="page-size-selector__body__typical-name body-3"
+          span(class="page-size-selector__body__typical body-3"
                 :class="selectedFormatKey === `preset-${index}` ? 'text-black' : defaultTextColor") {{ makeFormatString(format)}}
 </template>
 
@@ -270,7 +266,19 @@ export default Vue.extend({
         justify-content: center;
       }
       &.first-row {
+        display: block;
         margin-top: 0px;
+        font-family: 'SF Pro';
+        color: setColor(gray-2);
+        letter-spacing: 1.5px;
+      }
+      &.item {
+        display: grid;
+        margin: 0px;
+        padding: 4px 16px;
+        grid-template-columns: 28px auto;
+        justify-content: left;
+        line-height: 20px;
       }
     }
     &__title {
@@ -281,16 +289,23 @@ export default Vue.extend({
       display: grid;
       grid-template-columns: 1fr auto 1fr;
       grid-template-rows: auto;
-      column-gap: 18px;
+      column-gap: 15px;
       align-items: center;
       &__box {
-        height: 26px;
+        width: 128px;
+        height: 30px;
         box-sizing: border-box;
         padding: 5px 5px;
+        &__input-label {
+          width: 30px;
+          font-family: 'Manrope';
+          font-weight: 700;
+          text-align: center;
+        }
         & input {
           line-height: 16px;
           background-color: transparent;
-          font-family: Mulish;
+          font-family: 'Mulish';
           &::placeholder{
             color: setColor(gray-3);
           }
@@ -302,27 +317,17 @@ export default Vue.extend({
     }
     &__hr {
       width: 100%;
-      margin-top: 24px;
-      margin-bottom: 24px;
+      margin-top: 20px;
+      margin-bottom: 20px;
     }
     &__text {
-      font-family: Mulish;
-    }
-    &__recently {
-      @extend .page-size-selector__body__text;
-      width: 88%;
-    }
-    &__typical-name {
-      @extend .page-size-selector__body__text;
-      width: 88%;
-    }
-    &__typical-size {
-      @extend .page-size-selector__body__text;
-      width: 45%;
-      white-space: nowrap;
+      font-family: 'Mulish';
     }
   }
   &__container {
+    display: grid;
+    gap: 4px;
+    height: 243px;
     overflow-y: auto; // overlay is not supported in Firefox
     scrollbar-width: thin;
     &::-webkit-scrollbar {
--- src/components/popup/PopupSize.vue
+++ src/components/popup/PopupSize.vue
@@ -5,14 +5,15 @@
       svg-icon(class="pointer"
         iconName="page-close"
         iconWidth="10px"
-        iconColor="gray-0"
+        iconColor="gray-2"
         @click.native="closePopup()")
-    div(class="label-mid pb-20 text-center") {{$t('NN0072')}}
+    div(class="popup-size__title label-mid mb-20 text-center")
+      span {{$t('NN0072')}}
     PageSizeSelector(:isValidate="isConfirmClicked" defaultFormat="custom" ref="pageSizeSelector" @select="selectFormat")
     div(class="popup-size__body__button")
-      btn(:type="'primary-sm'"
-        class="rounded my-5 full-width pointer"
-        @click.native="onConfirmClicked()") {{$tc('NN0164', 2)}}
+      button(class="btn-primary-sm rounded my-6 full-width pointer"
+        @click="onConfirmClicked()")
+        span {{$tc('NN0164', 2)}}
 </template>
 
 <script lang="ts">
@@ -74,20 +75,42 @@ export default Vue.extend({
   border-radius: 5px;
   box-shadow: 0px 4px 13px rgba(0, 0, 0, 0.25);
   background-color: setColor(white);
-  padding: 20px 50px;
+  padding: 8px 67px 20px 67px;
+  &__title {
+    height: 26px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: 'SF Pro';
+    color: setColor(gray-2);
+    letter-spacing: 0.255em;
+    text-indent: 0.255em;
+  }
   &__body {
     &__button {
+      height: 30px;
+      align-items: center;
+      justify-content: center;
       margin: 0 auto;
-      width: 60%;
-      padding-top: 30px;
-      font-family: 'SF Pro';
-      font-weight: 400;
+      width: 240px;
+      padding-top: 20px;
+      span{
+        font-family: 'SF Pro';
+        font-weight: 400;
+        letter-spacing: 1.21em;
+        text-indent: 1.21em;
+      }
     }
   }
   &__close {
+    display: flex;
+    align-items: center;
+    justify-content: center;
     position: absolute;
-    top: 20px;
-    right: 20px;
+    top: 11px;
+    right: 13px;
+    width: 20px;
+    height: 20px;
   }
 }
 </style>
