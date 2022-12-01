671b01f91eb1150d70a6c7d9ec580ed1e0e3fc37
Nathan <nuphoto.nathan@gmail.com>
2022-12-01 16:09:11 +0800

    feat: Nubtn and NubtnList

---
+++ src/components/global/Nubtn.vue
@@ -0,0 +1,239 @@
+<template lang="pug">
+div(:class="`nubtn ${theme} ${sizeClass} ${status}`"
+    @mouseenter="mouseenter"
+    @mouseleave="mouseleave"
+    @click="click")
+  svg-icon(v-if="theme.includes('icon')"
+          :iconName="icon" iconWidth="24px" :iconColor="iconColor")
+  span(v-if="theme !== 'icon'")
+    slot
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Nubtn',
+  model: { // Let status prop and @status will be the target of v-model
+    prop: 'status',
+    event: 'status'
+  },
+  props: {
+    // The same as style in figma, but style is reserved prop name.
+    // Value: primary, outline, text, icon_text, icon, ghost, ghost_outline, danger, secondary
+    theme: {
+      type: String,
+      default: 'primary'
+    },
+    // Value: sm, sm-full, mid, mid-full
+    // full mean button will occupy all width.
+    size: {
+      type: String,
+      default: 'sm'
+    },
+    icon: {
+      type: String,
+      default: 'none'
+    },
+    // Use v-model if parent compenent need Nubtn status value.
+    // Use :status if you don't want Nubtn change status.
+    // Vale: default, hover, disabled, active
+    status: {
+      type: String,
+      default: 'default'
+    }
+  },
+  data() {
+    return {
+      hover: false,
+      active: false
+    }
+  },
+  computed: {
+    sizeClass(): string {
+      return this.size.replace('-', ' ')
+    },
+    iconColor(): string {
+      return this.theme === 'icon_text' ? 'white'
+        : this.status === 'disabled' ? 'gray-4' : 'gray-2'
+    }
+  },
+  methods: {
+    updateStatus() {
+      const newStatus = this.status === 'disabled' ? 'disabled'
+        : this.hover ? 'hover'
+          : this.active ? 'active'
+            : 'default'
+      this.$emit('status', newStatus)
+    },
+    mouseenter() {
+      this.hover = true
+      this.updateStatus()
+    },
+    mouseleave() {
+      this.hover = false
+      this.updateStatus()
+    },
+    click() {
+      this.active = !this.active
+      this.updateStatus()
+      this.$emit('click')
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.nubtn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 4px;
+  box-sizing: border-box;
+  cursor: pointer;
+  user-select: none;
+  &:not(.full) {
+    margin: auto;
+  }
+}
+
+@mixin default-size {
+  &.sm {
+    @include btn-SM;
+    height: 32px;
+    padding: 4px 16px;
+  }
+  &.mid {
+    @include btn-LG;
+    height: 44px;
+    padding: 10px 24px;
+  }
+}
+
+.default {
+  --blue: #{setColor(blue-1)};
+}
+.hover {
+  --blue: #{setColor(blue-hover)};
+}
+.disabled {
+  --blue: #{setColor(gray-4)};
+}
+.active {
+  --blue: #{setColor(blue-active)};
+}
+
+.primary {
+  @include default-size;
+  color: setColor(white);
+  background-color: var(--blue);
+}
+.outline {
+  @include default-size;
+  color: var(--blue);
+  border: 1px solid var(--blue);
+}
+.text {
+  @include default-size;
+  color: var(--blue);
+}
+.icon_text {
+  svg {
+    margin-right: 8px;
+  }
+  &.sm {
+    height: 36px;
+    padding: 6px 16px 6px 12px;
+  }
+  &.mid {
+    height: 44px;
+    padding: 10px 20px 10px 12px;
+  }
+  color: setColor(white);
+  background-color: var(--blue);
+}
+.icon {
+  width: 32px;
+  height: 32px;
+  &.hover {
+    background-color: setColor(blue-3, 0.5);
+  }
+  &.disabled {
+    opacity: 50%;
+  }
+  &.active {
+    background-color: setColor(blue-3);
+  }
+}
+.ghost {
+  height: 36px;
+  padding: 6px 24px;
+  border-radius: 50px;
+  &.default {
+    color: setColor(blue-1);
+    background-color: setColor(blue-4);
+  }
+  &.hover {
+    color: setColor(blue-1);
+    background-color: setColor(white);
+  }
+  &.disabled {
+    color: setColor(gray-4);
+    background-color: setColor(white);
+  }
+}
+.ghost_outline {
+  height: 36px;
+  padding: 6px 24px;
+  border-radius: 50px;
+  &.default {
+    color: setColor(blue-3);
+    border: 1px solid setColor(blue-3);
+  }
+  &.hover {
+    color: setColor(white);
+    border: 1px solid setColor(white);
+  }
+  &.disabled {
+    color: setColor(gray-4);
+    border: 1px solid setColor(gray-4);
+  }
+}
+.danger {
+  @include default-size;
+  color: setColor(white);
+  &.default {
+    background-color: setColor(red);
+  }
+  &.hover {
+    background-color: #FC5757;
+  }
+  &.disabled {
+    background-color: setColor(gray-4);
+  }
+  &.active {
+    background-color: #D9624E;
+  }
+}
+.secondary {
+  @include default-size;
+  &.default {
+    color: setColor(gray-2);
+    border: 1px solid setColor(gray-3);
+  }
+  &.hover {
+    color: setColor(gray-2);
+    background-color: setColor(gray-4);
+    border: 1px solid setColor(gray-3);
+  }
+  &.disabled {
+    color: setColor(white);
+    background-color: setColor(gray-4);
+    border: none;
+  }
+  &.active {
+    color: setColor(gray-2);
+    border: 1px solid setColor(gray-2);
+  }
+}
+</style>
---
+++ src/views/NubtnList.vue
@@ -0,0 +1,101 @@
+<template lang="pug">
+div(class="btnList" :style="{backgroundColor: darkBG ? '#2C2F43' : 'white'}")
+  div(class="btnList-options")
+    Checkbox(v-model="width100") width: 100%
+    Checkbox(v-model="darkBG") dark BG
+    div
+      span {{'button text: '}}
+      input(type="type" v-model="btnText")
+    div
+      span {{'button icon: '}}
+      input(type="type" v-model="btnIcon")
+  div(class="btnList-table")
+    span
+    span(v-for="btn in btns") {{btn.theme}} ({{btn.size}})
+    template(v-for="status in statuses")
+      span {{status}}
+      Nubtn(v-for="btn in btns"
+            :theme="btn.theme" :size="`${btn.size}${width100?'-full':''}`"
+            :status="status" :icon="btnIcon") {{btnText}}
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Nubtn from '@/components/global/Nubtn.vue'
+import Checkbox from '@/components/global/Checkbox.vue'
+
+export default Vue.extend({
+  name: 'NubtnList',
+  components: {
+    Nubtn,
+    Checkbox
+  },
+  data() {
+    return {
+      btns: [{
+        theme: 'primary',
+        size: 'sm'
+      }, {
+        theme: 'primary',
+        size: 'mid'
+      }, {
+        theme: 'icon_text',
+        size: 'sm'
+      }, {
+        theme: 'icon_text',
+        size: 'mid'
+      }, {
+        theme: 'outline',
+        size: 'sm'
+      }, {
+        theme: 'outline',
+        size: 'mid'
+      }, {
+        theme: 'text',
+        size: 'sm'
+      }, {
+        theme: 'ghost',
+        size: 'sm'
+      }, {
+        theme: 'ghost_outline',
+        size: 'sm'
+      }, {
+        theme: 'icon',
+        size: 'sm'
+      }, {
+        theme: 'danger',
+        size: 'sm'
+      }, {
+        theme: 'secondary',
+        size: 'sm'
+      }],
+      statuses: ['default', 'hover', 'disabled', 'active'],
+      width100: false,
+      darkBG: true,
+      btnText: 'button',
+      btnIcon: 'download'
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.btnList {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  overflow-y: auto;
+  &-options input {
+    width: 100px;
+  }
+  &-table {
+    display: grid;
+    align-items: center;
+    grid-auto-flow: column;
+    grid-template-rows: repeat(13, auto);
+    gap: 10px;
+    width: fit-content;
+  }
+}
+</style>
