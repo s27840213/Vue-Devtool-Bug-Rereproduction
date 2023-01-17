f5f823d05bbf3a0b746e1ad58106b5dee22074b7
ZhengYan <nuphoto.zhengyan@gmail.com>
2023-01-17 15:15:09 +0800

    test: remove nu-adjust-image

--- src/components/editor/global/NuBgImage.vue
+++ src/components/editor/global/NuBgImage.vue
@@ -1,12 +1,12 @@
 <template lang="pug">
   div(v-if="!image.config.imgContorl" class="nu-background-image" draggable="false" :style="mainStyles")
     div(v-show="!isColorBackground && !(isBgImgCtrl && imgControlPageIdx === pageIndex)" class="nu-background-image__image" :style="imgStyles()")
-      nu-adjust-image(v-if="isAdjustImage"
-        :src="finalSrc"
-        :styles="adjustImgStyles"
-        :contentScaleRatio="contentScaleRatio"
-        @error="onError")
-      img(v-else-if="src"
+      //- nu-adjust-image(v-if="isAdjustImage"
+      //-   :src="finalSrc"
+      //-   :styles="adjustImgStyles"
+      //-   :contentScaleRatio="contentScaleRatio"
+      //-   @error="onError")
+      img(
         :src="finalSrc"
         draggable="false"
         class="body"
