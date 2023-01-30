67d84c9293f3499e2074c058292ed3e3591ae34e
Nathan <nuphoto.nathan@gmail.com>
2023-01-30 11:22:48 +0800

    cy: login from email

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,7 +1,17 @@
+import loginData from '../fixtures/loginData.json'
+
 describe('Testing login', () => {
-  it('login from email', () => {
+  it('Login from POST request', () => {
     cy.login()
-    cy.visit('')
+    cy.visit('/')
+    cy.get('.avatar').should('exist')
+  })
+
+  it('Login from email', () => {
+    cy.visit('/login')
+    cy.get('input[type="email"]').type(loginData.email.account)
+    cy.get('input[type="password"]').type(loginData.email.password)
+    cy.get('button').contains('登 入').click()
     cy.get('.avatar').should('exist')
   })
 })
