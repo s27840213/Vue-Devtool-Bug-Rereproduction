841af676366d780224d4e6c344b59c0a964eae7b
ZhengYan <nuphoto.zhengyan@gmail.com>
2023-02-01 11:20:16 +0800

    fix: add -webkit-filter: opacity(1) to halation

--- src/utils/imageAdjustUtil.ts
+++ src/utils/imageAdjustUtil.ts
@@ -201,7 +201,8 @@ class ImageAdjustUtil {
       top: 0,
       left: 0,
       right: 0,
-      bottom: 0
+      bottom: 0,
+      ...(store.getters['user/getUserId'] === 'backendRendering' && { '-webkit-filter': 'opacity(1)' })
     }
     return [{
       tag: 'div',
