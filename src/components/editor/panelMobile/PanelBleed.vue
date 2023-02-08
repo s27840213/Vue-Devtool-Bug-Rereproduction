5c80992383a4b34c9fc1b9eda8445d396208efe9
ZhengYan <nuphoto.zhengyan@gmail.com>
2023-02-08 17:01:16 +0800

    fix: pass page to bleed settings

--- src/components/editor/panelMobile/PanelBleed.vue
+++ src/components/editor/panelMobile/PanelBleed.vue
@@ -2,16 +2,23 @@
 div(class="panel-bleed text-left")
   span(class="subtitle-2 text-gray-2") {{$t('NN0780')}}
   div(class="panel-bleed__content")
-    bleed-settings
+    bleed-settings(:page="currPage")
 </template>
 
 <script lang="ts">
 import BleedSettings from '@/components/editor/BleedSettings.vue'
-import { defineComponent } from 'vue'
+import { IPage } from '@/interfaces/page'
+import { defineComponent, PropType } from 'vue'
 
 export default defineComponent({
   components: {
     BleedSettings
+  },
+  props: {
+    currPage: {
+      type: Object as PropType<IPage>,
+      required: true
+    }
   }
 })
 </script>
