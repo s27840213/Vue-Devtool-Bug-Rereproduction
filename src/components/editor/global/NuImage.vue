36fe240d9252f55e52f8d05886891c5109baea92
steve <nuphoto.steve@gmail.com>
2023-02-18 10:13:01 +0800

    feat: some adjust structure modified

--- src/components/editor/global/NuImage.vue
+++ src/components/editor/global/NuImage.vue
@@ -40,13 +40,12 @@ div(v-if="!config.imgControl || forRender || isBgImgControl" class="nu-image"
                 :key="child.tag"
                 :is="child.tag"
                 v-bind="child.attrs")
-        g
-          g(:filter="`url(#${filterId})`")
-            image(:xlink:href="finalSrc" ref="img"
-              class="nu-image__picture"
-              draggable="false"
-              @error="onError"
-              @load="onLoad")
+        image(:xlink:href="finalSrc" ref="img"
+          :filter="`url(#${filterId})`"
+          class="nu-image__picture"
+          draggable="false"
+          @error="onError"
+          @load="onLoad")
       img(v-else-if="src" ref="img"
         :style="flipStyles()"
         :class="{'nu-image__picture': true, 'layer-flip': flippedAnimation() }"
@@ -333,11 +332,15 @@ export default defineComponent({
       }
       return this.src
     },
+    // filterId(): string {
+    //   const { styles: { adjust }, id: layerId } = this.config
+    //   const { blur = 0, brightness = 0, contrast = 0, halation = 0, hue = 0, saturate = 0, warm = 0 } = adjust
+    //   const id = layerId + blur.toString() + brightness.toString() + contrast.toString() + halation.toString() + hue.toString() + saturate.toString() + warm.toString()
+    //   return `filter__${id}`
+    // },
     filterId(): string {
-      const { styles: { adjust }, id: layerId } = this.config
-      const { blur = 0, brightness = 0, contrast = 0, halation = 0, hue = 0, saturate = 0, warm = 0 } = adjust
-      const id = layerId + blur.toString() + brightness.toString() + contrast.toString() + halation.toString() + hue.toString() + saturate.toString() + warm.toString()
-      return `filter__${id}`
+      const randomId = generalUtils.generateRandomString(5)
+      return `filter__${randomId}`
     },
     showCanvas(): boolean {
       const { subLayerIndex, handleId } = this
