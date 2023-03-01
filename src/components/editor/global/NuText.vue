362ab1b22a1515d1039e6b8bb5e1760272ae939b
TingAnYen <nuphoto.daniel@gmail.com>
2023-03-01 16:15:04 +0800

    fix: force layer added by addText autoResized at least once

--- src/components/editor/global/NuText.vue
+++ src/components/editor/global/NuText.vue
@@ -105,16 +105,7 @@ export default defineComponent({
     this.isDestroyed = true
   },
   mounted() {
-    // To solve the issues: https://www.notion.so/vivipic/8cbe77d393224c67a43de473cd9e8a24
-    textUtils.untilFontLoaded(this.config.paragraphs, true).then(() => {
-      setTimeout(() => {
-        this.resizeCallback()
-        if (this.$route.name === 'Editor' || this.$route.name === 'MobileEditor') {
-          this.isLoading = false
-        }
-      }, 100) // for the delay between font loading and dom rendering
-    })
-    this.drawSvgBG()
+    this.resizeAfterFontLoaded()
   },
   computed: {
     ...mapState(['isMoving']),
@@ -185,6 +176,13 @@ export default defineComponent({
       deep: true,
       handler() {
         this.drawSvgBG()
+      }
+    },
+    'config.isAutoResizeNeeded': {
+      handler(newVal) {
+        if (newVal) {
+          this.resizeAfterFontLoaded()
+        }
       }
     }
   },
@@ -367,6 +365,9 @@ export default defineComponent({
     isAutoResizeNeeded(): boolean {
       return this.page.isAutoResizeNeeded
     },
+    isLayerAutoResizeNeeded(): boolean {
+      return this.config.isAutoResizeNeeded
+    },
     styles(styles: any) {
       return tiptapUtils.textStylesRaw(styles)
     },
@@ -415,8 +416,9 @@ export default defineComponent({
       // console.log('resize')
 
       let widthLimit
-      if (this.isLoading && this.isAutoResizeNeeded()) {
+      if ((this.isLoading && this.isAutoResizeNeeded()) || this.isLayerAutoResizeNeeded()) {
         widthLimit = await textUtils.autoResize(config, this.initSize)
+        LayerUtils.updateLayerProps(this.pageIndex, this.layerIndex, { isAutoResizeNeeded: false }, this.subLayerIndex)
       } else {
         widthLimit = config.widthLimit
       }
@@ -446,6 +448,18 @@ export default defineComponent({
         const { width, height } = calcTmpProps(group.layers, group.styles.scale)
         LayerUtils.updateLayerStyles(this.pageIndex, this.layerIndex, { width, height })
       }
+      this.drawSvgBG()
+    },
+    resizeAfterFontLoaded() {
+      // To solve the issues: https://www.notion.so/vivipic/8cbe77d393224c67a43de473cd9e8a24
+      textUtils.untilFontLoaded(this.config.paragraphs, true).then(() => {
+        setTimeout(() => {
+          this.resizeCallback()
+          if (this.$route.name === 'Editor' || this.$route.name === 'MobileEditor') {
+            this.isLoading = false
+          }
+        }, 100) // for the delay between font loading and dom rendering
+      })
       this.drawSvgBG()
     }
   }
