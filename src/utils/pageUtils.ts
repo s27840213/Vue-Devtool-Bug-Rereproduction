fa4c94b251de5934e565f9c01f66de7e7a0bc969
Alan Chang <nuphoto.alan@gmail.com>
2023-03-20 16:30:56 +0800

    fix: fix the magnifier errors in mobile

--- src/utils/pageUtils.ts
+++ src/utils/pageUtils.ts
@@ -530,11 +530,10 @@ class PageUtils {
     // Target size can be pass by param or get according to situation.
     const editorViewBox = document.getElementsByClassName('editor-view')[0]
     const mobilePanelHeight = document.getElementsByClassName('mobile-panel')[0]?.clientHeight ?? 0
-
     if (!editorViewBox) return
     let { clientWidth: editorWidth, clientHeight: editorHeight } = editorViewBox
     const { width: targetWidth, height: targetHeight }: { width: number, height: number } =
-      (this.inBgRemoveMode ? this.autoRemoveResult
+      (this.inBgRemoveMode ? { width: 1600, height: this.autoRemoveResult.height * (1600 / this.autoRemoveResult.width) }
         : (this.hasBleed ? this.currFocusPageSizeWithBleeds : this.currFocusPageSize))
 
     const RESIZE_MULTIPLIER = this.isMobile ? 1 : 0.8
