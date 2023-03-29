2ee717a99cde49bf83ceff9667dac9a10ab9f2d4
TingAnYen <nuphoto.daniel@gmail.com>
2023-03-29 21:57:55 +0800

    fix: fix controllers tooSmall criteria for contentScaleRatio and refactor

--- src/components/editor/global/NuController.vue
+++ src/components/editor/global/NuController.vue
@@ -475,6 +475,15 @@ export default defineComponent({
       setImgConfig: 'imgControl/SET_CONFIG',
       setBgConfig: 'imgControl/SET_BG_CONFIG'
     }),
+    checkLimit(dimension: number, limit = RESIZER_SHOWN_MIN) {
+      return dimension * this.scaleRatio * this.contentScaleRatio < limit
+    },
+    checkLimits(limit = RESIZER_SHOWN_MIN): { tooShort: boolean, tooNarrow: boolean } {
+      return {
+        tooShort: this.checkLimit(this.getLayerHeight(), limit),
+        tooNarrow: this.checkLimit(this.getLayerWidth(), limit)
+      }
+    },
     addMovingListener() {
       const body = (this.$refs.body as HTMLElement[])[0]
       const lineMover = this.$refs.lineMover as HTMLElement
@@ -532,16 +541,15 @@ export default defineComponent({
       const scalerOffset = this.$isTouchDevice() ? 36 : 20
       const HW = {
         // Get the widht/height of the controller for resizer-bar and minus the scaler size
-        width: isHorizon ? `${(this.getLayerWidth() - scalerOffset) * this.scaleRatio * 0.01}px` : `${width * this.contentScaleRatio * this.scaleRatio * 0.01}px`,
-        height: !isHorizon ? `${(this.getLayerHeight() - scalerOffset) * this.scaleRatio * 0.01}px` : `${height * this.contentScaleRatio * this.scaleRatio * 0.01}px`,
+        width: isHorizon ? `${(this.getLayerWidth() - scalerOffset) * this.contentScaleRatio * this.scaleRatio * 0.01}px` : `${width * this.contentScaleRatio * this.scaleRatio * 0.01}px`,
+        height: !isHorizon ? `${(this.getLayerHeight() - scalerOffset) * this.contentScaleRatio * this.scaleRatio * 0.01}px` : `${height * this.contentScaleRatio * this.scaleRatio * 0.01}px`,
         opacity: 0
       }
       return Object.assign(resizerStyle, HW)
     },
     resizerStyles(resizer: IResizer, isTouchArea = false) {
       const resizerStyle = { ...resizer }
-      const tooShort = this.getLayerHeight() * this.scaleRatio < RESIZER_SHOWN_MIN
-      const tooNarrow = this.getLayerWidth() * this.scaleRatio < RESIZER_SHOWN_MIN
+      const { tooShort, tooNarrow } = this.checkLimits()
       const tooSmall = this.getLayerType === 'text'
         ? (this.config.styles.writingMode.includes('vertical') ? tooNarrow : tooShort) : false
       if (!tooSmall) {
@@ -568,8 +576,7 @@ export default defineComponent({
     },
     getResizer(controlPoints: ICP, textMoveBar = false, isTouchArea = false) {
       let resizers = isTouchArea ? controlPoints.resizerTouchAreas : controlPoints.resizers
-      const tooShort = this.getLayerHeight() * this.scaleRatio < RESIZER_SHOWN_MIN
-      const tooNarrow = this.getLayerWidth() * this.scaleRatio < RESIZER_SHOWN_MIN
+      const { tooShort, tooNarrow } = this.checkLimits()
       switch (this.getLayerType) {
         case 'image':
           resizers = this.config.styles.shadow.currentEffect === ShadowEffectType.none ? resizers : []
@@ -620,14 +627,12 @@ export default defineComponent({
     },
     getScaler(scalers: any) {
       const LIMIT = (this.getLayerType === 'text') ? RESIZER_SHOWN_MIN : RESIZER_SHOWN_MIN / 2
-      const tooShort = this.getLayerHeight() * this.scaleRatio < LIMIT
-      const tooNarrow = this.getLayerWidth() * this.scaleRatio < LIMIT
+      const { tooShort, tooNarrow } = this.checkLimits(LIMIT)
       return (tooShort || tooNarrow) ? scalers.slice(2, 3) : scalers
     },
     getCornerRotaters(scalers: any) {
       const LIMIT = (this.getLayerType === 'text') ? RESIZER_SHOWN_MIN : RESIZER_SHOWN_MIN / 2
-      const tooShort = this.getLayerHeight() * this.scaleRatio < LIMIT
-      const tooNarrow = this.getLayerWidth() * this.scaleRatio < LIMIT
+      const { tooShort, tooNarrow } = this.checkLimits(LIMIT)
       return (tooShort || tooNarrow) ? scalers.slice(2, 3) : scalers
     },
     lineEnds(scalers: any, point: number[]) {
@@ -1668,8 +1673,7 @@ export default defineComponent({
       }
       this.setCursorStyle((event.target as HTMLElement).style.cursor || 'move')
       const LIMIT = (this.getLayerType === 'text') ? RESIZER_SHOWN_MIN : RESIZER_SHOWN_MIN / 2
-      const tooShort = this.getLayerHeight() * this.scaleRatio < LIMIT
-      const tooNarrow = this.getLayerWidth() * this.scaleRatio < LIMIT
+      const { tooShort, tooNarrow } = this.checkLimits(LIMIT)
       if (tooShort || tooNarrow) {
         index = 2
       }
@@ -1834,8 +1838,7 @@ export default defineComponent({
       if (typeof index === 'number') {
         if (type === 'cornerRotaters') {
           const LIMIT = (this.getLayerType === 'text') ? RESIZER_SHOWN_MIN : RESIZER_SHOWN_MIN / 2
-          const tooShort = this.getLayerHeight() * this.scaleRatio < LIMIT
-          const tooNarrow = this.getLayerWidth() * this.scaleRatio < LIMIT
+          const { tooShort, tooNarrow } = this.checkLimits(LIMIT)
           if (tooShort || tooNarrow) {
             index = 2
           }
