520de1838a8d290594ec0c474457a3aa1f600034
ZhengYan <nuphoto.zhengyan@gmail.com>
2023-04-12 16:25:26 +0800

    refactor: remove redundant codes

--- src/components/vivisticker/FullPage.vue
+++ src/components/vivisticker/FullPage.vue
@@ -111,39 +111,5 @@ export default defineComponent({
       object-fit: contain;
     }
   }
-  &__welcome {
-    overflow-y: auto;
-    display: flex;
-    flex-direction: column;
-    >img {
-      position: relative;
-      top: 10%;
-      width: 100%;
-      max-height: 50%;
-      object-fit: contain;
-    }
-    &__text {
-      position: relative;
-      top: 15%;
-      margin: 0 24px;
-    }
-    &__btn-start {
-      margin: 0 auto;
-      position: absolute;
-      left: 50%;
-      transform: translateX(-50%);
-      bottom: 38px;
-      display: flex;
-      flex-direction: row;
-      justify-content: center;
-      align-items: center;
-      padding: 4px 16px;
-      background: #FFFFFF;
-      border-radius: 10px;
-      font-weight: 600;
-      font-size: 16px;
-      line-height: 24px;
-    }
-  }
 }
 </style>
