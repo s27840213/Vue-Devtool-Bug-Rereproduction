3f9e4d4dc352c8946625b4b861c4dbfb45a7ef40
TingAnYen <nuphoto.daniel@gmail.com>
2023-05-15 16:36:26 +0800

    feat: limit querystring selection as single selection in template center

--- src/views/TemplateCenter.vue
+++ src/views/TemplateCenter.vue
@@ -308,8 +308,7 @@ export default defineComponent({
         }
         this.hashtagSelections[hashtag.title] = {
           type: hashtag.type,
-          // selection: selection.slice(0, 1) // limit querystring selection as single selection
-          selection
+          selection: selection.slice(0, 1) // limit querystring selection as single selection
         }
       }
       this.composeKeyword()
