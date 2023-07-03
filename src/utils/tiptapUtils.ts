2d7e3f22582439a93ddb3025d474d3e4179da184
TingAnYen <nuphoto.daniel@gmail.com>
2023-07-03 12:19:19 +0800

    fix: treat invalid mark.attrs as no marks

--- src/utils/tiptapUtils.ts
+++ src/utils/tiptapUtils.ts
@@ -296,8 +296,7 @@ class TiptapUtils {
           .map(t => Object.assign({}, span, { text: t })))
         : paragraph.content
       for (const span of pContent ?? []) {
-        if (span.marks && span.marks.length > 0) {
-          console.log(generalUtils.deepCopy(span.marks[0].attrs))
+        if (span.marks && span.marks.length > 0 && span.marks[0].attrs.font !== 'undefined') {
           const sStyles = this.makeSpanStyle(span.marks[0].attrs)
           if (sStyles.size > largestSize) largestSize = sStyles.size
           if (sStyles.pre && !span.text.match(/^ +$/)) {
