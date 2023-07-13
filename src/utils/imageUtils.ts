2481a0c585a420ce7521f2d60a5f104878368fbe
steve <nuphoto.steve@gmail.com>
2023-07-13 14:30:03 +0800

    update: update app_ver_for_refresh img

--- src/utils/imageUtils.ts
+++ src/utils/imageUtils.ts
@@ -15,7 +15,7 @@ import LayerUtils from './layerUtils'
 import mouseUtils from './mouseUtils'
 import pageUtils from './pageUtils'
 
-const APP_VER_FOR_REFRESH_CACHE = 'v7097'
+const APP_VER_FOR_REFRESH_CACHE = 'v7155'
 
 class ImageUtils {
   async imgLoadHandler<T>(src: string, cb: (img: HTMLImageElement) => T, options?: { error?: () => void, crossOrigin?: boolean }) {
