23a0563b4de1f3cf0d7250c56a742e099aa4a623
張祐禎 <s27840213@gmail.com>
2023-08-11 03:33:44 +0800

    feat(*): qwe
    qwe
    
    ISSUES CLOSED: #31

--- .cz-config.js
+++ .cz-config.js
@@ -3,16 +3,16 @@
 module.exports = {
 
 	types: [
-		{ value: 'feat', name: 'feat:     A new feature' },
+		{ value: 'feat', name: 'feat:     AAAA new feature' },
 		{ value: 'fix', name: 'fix:      A bug fix' },
-		{ value: 'docs', name: 'docs:     Documentation only changes' },
-		{ value: 'style', name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)' },
+		// { value: 'docs', name: 'docs:     Documentation only changes' },
+		// { value: 'style', name: 'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)' },
 		{ value: 'refactor', name: 'refactor: A code change that neither fixes a bug nor adds a feature' },
 		{ value: 'perf', name: 'perf:     A code change that improves performance' },
-		{ value: 'test', name: 'test:     Adding missing tests' },
-		{ value: 'chore', name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation' },
+		// { value: 'test', name: 'test:     Adding missing tests' },
+		// { value: 'chore', name: 'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation' },
 		{ value: 'revert', name: 'revert:   Revert to a commit' },
-		{ value: 'WIP', name: 'WIP:      Work in progress' }
+		// { value: 'WIP', name: 'WIP:      Work in progress' }
 	],
 
 	scopes: [
@@ -42,12 +42,12 @@ module.exports = {
 		customScope: 'Denote the SCOPE of this change:',
 		subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
 		body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
-		breaking: 'List any BREAKING CHANGES (optional):\n',
+		// breaking: 'List any BREAKING CHANGES (optional):\n',
 		footer: 'List any ISSUES CLOSED by this change (optional). E.g.: #31, #34:\n',
 		confirmCommit: 'Are you sure you want to proceed with the commit above?'
 	},
 
-	allowCustomScopes: true,
+	allowCustomScopes: false,
 	allowBreakingChanges: ['feat', 'fix', 'perf'],
 
 	// limit subject length
