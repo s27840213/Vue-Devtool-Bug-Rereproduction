1292cb5dca01f34988dbb7746542b53d7e95eb76
Alan Chang <nuphoto.alan@gmail.com>
2023-08-11 15:28:01 +0800

    feat: setup the global eslint, prettier, and .vscode settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,18 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
+    'plugin:vue-pug/vue3-recommended',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  // to prevent commitlint.config.js to throw error
+  env: {
+    node: true
   }
 }
--- projects/aipe/src/components/HelloWorld.vue
+++ projects/aipe/src/components/HelloWorld.vue
@@ -4,15 +4,18 @@ defineProps<{
 }>()
 </script>
 
-<template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
-  </div>
+<template lang="pug">
+div(class="greetings")
+  h1 {{  msg  }}
+  h3 You’ve successfully created a project with
+    a(
+      href="https://vitejs.dev/"
+      target="_blank"
+      rel="noopener") Vite +
+    a(
+      href="https://vuejs.org/"
+      target="_blank"
+      rel="noopener") Vue 3. What's next?
 </template>
 
 <style scoped>
