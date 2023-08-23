6c5a346f7b52a78b62211af93041f352d6e1e558
TingAnYen <nuphoto.daniel@gmail.com>
2023-08-23 16:52:24 +0800

    fix: change app to vivisticker

--- src/utils/autoWVUtils.ts
+++ src/utils/autoWVUtils.ts
@@ -1,5 +1,5 @@
-import picWVUtils from '@/utils/picWVUtils'
-// import vivistickerUtils from '@/utils/vivistickerUtils' // uncomment this line in vivisticker
+// import picWVUtils from '@/utils/picWVUtils' // comment out this line in vivisticker
+import vivistickerUtils from '@/utils/vivistickerUtils' // uncomment this line in vivisticker
 
 export enum appType { // TODO: somehow detect app type
   Vivipic,
@@ -7,8 +7,8 @@ export enum appType { // TODO: somehow detect app type
   Charmix
 }
 
-export const app = appType.Vivipic as appType // comment out this line in vivisticker
-// export const app = appType.Vivisticker // uncomment this line in vivisticker
+// export const app = appType.Vivipic as appType // comment out this line in vivisticker
+export const app = appType.Vivisticker as appType // uncomment this line in vivisticker
 
-export default picWVUtils // comment out this line in vivisticker
-// export default vivistickerUtils // uncomment this line in vivisticker
+// export default picWVUtils // comment out this line in vivisticker
+export default vivistickerUtils // uncomment this line in vivisticker
