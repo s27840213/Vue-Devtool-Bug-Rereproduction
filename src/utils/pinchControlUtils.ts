b415fa8788cb851686cf9c7c192ad74f04869d85
steve <nuphoto.steve@gmail.com>
2023-09-16 17:23:12 +0800

    feat: add scale, moving logic

--- src/utils/pinchControlUtils.ts
+++ src/utils/pinchControlUtils.ts
@@ -1,13 +1,16 @@
 import { ILayer } from '@/interfaces/layer'
+import { ISize } from '@/interfaces/math'
 import store from '@/store'
 import { ILayerInfo } from '@/store/types'
+import layerUtils from '@/utils/layerUtils'
 import { AnyTouchEvent } from 'any-touch'
 
 export default class PinchControlUtils {
   private init = null as null | {
     pos: { x: number, y: number },
     layerPos: { x: number, y: number }
-    scale: number
+    scale: number,
+    size: ISize
   }
 
   private layerInfo: ILayerInfo
@@ -35,10 +38,36 @@ export default class PinchControlUtils {
       this.init = {
         pos: { x: e.x, y: e.y },
         layerPos: { x: this.config.styles.x, y: this.config.styles.y },
+        size: { width: this.config.styles.width, height: this.config.styles.height },
         scale: this.config.styles.scale
       }
     }
-    console.log('moving', e)
+    const newScale = e.scale * this.init.scale
+    const newSize = {
+      width: this.init.size.width * e.scale,
+      height: this.init.size.height * e.scale
+    }
+    const movingTranslate = {
+      x: e.x - this.init.pos.x,
+      y: e.y - this.init.pos.y
+    }
+    const compensateTranslate = {
+      x: (this.init.size.width - newSize.width) * 0.5,
+      y: (this.init.size.height - newSize.height) * 0.5
+    }
+    const totalTranslate = {
+      x: movingTranslate.x + compensateTranslate.x,
+      y: movingTranslate.y + compensateTranslate.y
+    }
+    layerUtils.updateLayerStyles(this.layerInfo.pageIndex, this.layerInfo.layerIndex, {
+      scale: newScale,
+      width: newSize.width,
+      height: newSize.height,
+      x: this.init.layerPos.x + totalTranslate.x,
+      y: this.init.layerPos.y + totalTranslate.y
+    })
+    console.log('moving', e.scale, this.init.layerPos.x + totalTranslate.x, this.init.layerPos.y + totalTranslate.y)
+    console.log(this.init.layerPos.x, this.init.layerPos.y)
   }
 
   end(e: AnyTouchEvent) {
