496be8901f241441ce89792fec7ea79aaa5d8824
Alan Chang <nuphoto.alan@gmail.com>
2023-09-19 16:40:03 +0800

    fix: delete the duplicate tool files in bash folder

--- tools/bash/create_pull_request.sh
+++ tools/bash/create_pull_request.sh
@@ -2,124 +2,204 @@
 apt-get update
 apt-get -y install curl jq
 export BITBUCKET_TRIGGERER_USERNAME=$(curl -X GET -g "https://api.bitbucket.org/2.0/users/${BITBUCKET_STEP_TRIGGERER_UUID}" | jq --raw-output '.display_name')
-if [[ "$BITBUCKET_BRANCH" == "develop" ]]
-then
-    echo "create pull request to qa"
-    curl --request POST \
-        --url "https://api.bitbucket.org/2.0/repositories/mingchi/nueditor/pullrequests" \
-        --header "Accept: application/json" \
-        --header "Content-Type: application/json" \
-        --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
-        --data '{
-          "title": "Pull request to qa by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
-          "source": {
-            "branch": {
-              "name": "'"${BITBUCKET_BRANCH}"'"
-            }
-          },
-          "destination": {
-            "branch": {
-              "name": "qa"
-            }
-          }
-        }'
-elif [[ "$BITBUCKET_BRANCH" == hotfix* ]]
-then
-    echo "create pull request to master, develop, and qa"
-    curl --request POST \
-        --url "https://api.bitbucket.org/2.0/repositories/mingchi/nueditor/pullrequests" \
-        --header "Accept: application/json" \
-        --header "Content-Type: application/json" \
-        --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
-        --data '{
-          "title": "Pull request to master by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
-          "source": {
-            "branch": {
-              "name": "'"${BITBUCKET_BRANCH}"'"
-            }
-          },
-          "destination": {
-            "branch": {
-              "name": "master"
-            }
-          }
-        }'
-    curl --request POST \
-        --url "https://api.bitbucket.org/2.0/repositories/mingchi/nueditor/pullrequests" \
-        --header "Accept: application/json" \
-        --header "Content-Type: application/json" \
-        --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
-        --data '{
-          "title": "Pull request to develop by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
-          "source": {
-            "branch": {
-              "name": "'"${BITBUCKET_BRANCH}"'"
-            }
-          },
-          "destination": {
-            "branch": {
-              "name": "develop"
-            }
-          }
-        }'
-    curl --request POST \
-        --url "https://api.bitbucket.org/2.0/repositories/mingchi/nueditor/pullrequests" \
-        --header "Accept: application/json" \
-        --header "Content-Type: application/json" \
-        --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
-        --data '{
-          "title": "Pull request to qa by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
-          "source": {
-            "branch": {
-              "name": "'"${BITBUCKET_BRANCH}"'"
-            }
-          },
-          "destination": {
-            "branch": {
-              "name": "qa"
-            }
-          }
-        }'
-elif [[ "$BITBUCKET_BRANCH" == "qa" ]]
-then
-    echo "create pull request to master"
-    curl --request POST \
-        --url "https://api.bitbucket.org/2.0/repositories/mingchi/nueditor/pullrequests" \
-        --header "Accept: application/json" \
-        --header "Content-Type: application/json" \
-        --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
-        --data '{
-          "title": "Pull request to master by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
-          "source": {
-            "branch": {
-              "name": "'"${BITBUCKET_BRANCH}"'"
-            }
-          },
-          "destination": {
-            "branch": {
-              "name": "master"
-            }
-          }
-        }'
-else
-    echo "create pull request to develop"
-    curl --request POST \
-        --url "https://api.bitbucket.org/2.0/repositories/mingchi/nueditor/pullrequests" \
-        --header "Accept: application/json" \
-        --header "Content-Type: application/json" \
-        --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
-        --data '{
-          "title": "Pull request to develop by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
-          "source": {
-            "branch": {
-              "name": "'"${BITBUCKET_BRANCH}"'"
-            }
-          },
-          "destination": {
-            "branch": {
-              "name": "develop"
-            }
-          }
-        }'
-fi
+echo "create pull request to develop"
+curl --request POST \
+    --url "https://api.bitbucket.org/2.0/repositories/mingchi/frontend-web/pullrequests" \
+    --header "Accept: application/json" \
+    --header "Content-Type: application/json" \
+    --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
+    --data '{
+      "title": "Pull request to develop by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
+      "source": {
+        "branch": {
+          "name": "'"${BITBUCKET_BRANCH}"'"
+        }
+      },
+      "destination": {
+        "branch": {
+          "name": "develop"
+        }
+      }
+    }'
+
+# if [[ "$BITBUCKET_BRANCH" == "app/vivisticker-develop" ]]
+# then
+#     echo "create pull request to vivisticker"
+#     curl --request POST \
+#         --url "https://api.bitbucket.org/2.0/repositories/mingchi/frontend-web/pullrequests" \
+#         --header "Accept: application/json" \
+#         --header "Content-Type: application/json" \
+#         --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
+#         --data '{
+#           "title": "Pull request to vivisticker by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
+#           "source": {
+#             "branch": {
+#               "name": "'"${BITBUCKET_BRANCH}"'"
+#             }
+#           },
+#           "destination": {
+#             "branch": {
+#               "name": "app/vivisticker"
+#             }
+#           }
+#         }'
+# elif [[ "$BITBUCKET_BRANCH" == "develop" ]]
+# then
+#     echo "create pull request to qa"
+#     curl --request POST \
+#         --url "https://api.bitbucket.org/2.0/repositories/mingchi/frontend-web/pullrequests" \
+#         --header "Accept: application/json" \
+#         --header "Content-Type: application/json" \
+#         --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
+#         --data '{
+#           "title": "Pull request to qa by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
+#           "source": {
+#             "branch": {
+#               "name": "'"${BITBUCKET_BRANCH}"'"
+#             }
+#           },
+#           "destination": {
+#             "branch": {
+#               "name": "qa"
+#             }
+#           }
+#         }'
+# elif [[ "$BITBUCKET_BRANCH" == hotfix* ]]
+# then
+#     echo "create pull request to master, develop, and qa"
+#     curl --request POST \
+#         --url "https://api.bitbucket.org/2.0/repositories/mingchi/frontend-web/pullrequests" \
+#         --header "Accept: application/json" \
+#         --header "Content-Type: application/json" \
+#         --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
+#         --data '{
+#           "title": "Pull request to master by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
+#           "source": {
+#             "branch": {
+#               "name": "'"${BITBUCKET_BRANCH}"'"
+#             }
+#           },
+#           "destination": {
+#             "branch": {
+#               "name": "master"
+#             }
+#           }
+#         }'
+#     curl --request POST \
+#         --url "https://api.bitbucket.org/2.0/repositories/mingchi/frontend-web/pullrequests" \
+#         --header "Accept: application/json" \
+#         --header "Content-Type: application/json" \
+#         --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
+#         --data '{
+#           "title": "Pull request to develop by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
+#           "source": {
+#             "branch": {
+#               "name": "'"${BITBUCKET_BRANCH}"'"
+#             }
+#           },
+#           "destination": {
+#             "branch": {
+#               "name": "develop"
+#             }
+#           }
+#         }'
+#     curl --request POST \
+#         --url "https://api.bitbucket.org/2.0/repositories/mingchi/frontend-web/pullrequests" \
+#         --header "Accept: application/json" \
+#         --header "Content-Type: application/json" \
+#         --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
+#         --data '{
+#           "title": "Pull request to qa by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
+#           "source": {
+#             "branch": {
+#               "name": "'"${BITBUCKET_BRANCH}"'"
+#             }
+#           },
+#           "destination": {
+#             "branch": {
+#               "name": "qa"
+#             }
+#           }
+#         }'
+# elif [[ "$BITBUCKET_BRANCH" == "stk-hotfix" ]]
+# then
+#     echo "create pull request to app/vivisticker and app/vivisticker-develop"
+#     curl --request POST \
+#         --url "https://api.bitbucket.org/2.0/repositories/mingchi/frontend-web/pullrequests" \
+#         --header "Accept: application/json" \
+#         --header "Content-Type: application/json" \
+#         --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
+#         --data '{
+#           "title": "Pull request to vivisticker by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
+#           "source": {
+#             "branch": {
+#               "name": "'"${BITBUCKET_BRANCH}"'"
+#             }
+#           },
+#           "destination": {
+#             "branch": {
+#               "name": "app/vivisticker"
+#             }
+#           }
+#         }'
+#     curl --request POST \
+#         --url "https://api.bitbucket.org/2.0/repositories/mingchi/frontend-web/pullrequests" \
+#         --header "Accept: application/json" \
+#         --header "Content-Type: application/json" \
+#         --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
+#         --data '{
+#           "title": "Pull request to vivisticker-develop by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
+#           "source": {
+#             "branch": {
+#               "name": "'"${BITBUCKET_BRANCH}"'"
+#             }
+#           },
+#           "destination": {
+#             "branch": {
+#               "name": "app/vivisticker-develop"
+#             }
+#           }
+#         }'
+# elif [[ "$BITBUCKET_BRANCH" == "qa" ]]
+# then
+#     echo "create pull request to master"
+#     curl --request POST \
+#         --url "https://api.bitbucket.org/2.0/repositories/mingchi/frontend-web/pullrequests" \
+#         --header "Accept: application/json" \
+#         --header "Content-Type: application/json" \
+#         --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
+#         --data '{
+#           "title": "Pull request to master by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
+#           "source": {
+#             "branch": {
+#               "name": "'"${BITBUCKET_BRANCH}"'"
+#             }
+#           },
+#           "destination": {
+#             "branch": {
+#               "name": "master"
+#             }
+#           }
+#         }'
+# else
+#     echo "create pull request to develop"
+#     curl --request POST \
+#         --url "https://api.bitbucket.org/2.0/repositories/mingchi/frontend-web/pullrequests" \
+#         --header "Accept: application/json" \
+#         --header "Content-Type: application/json" \
+#         --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
+#         --data '{
+#           "title": "Pull request to develop by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
+#           "source": {
+#             "branch": {
+#               "name": "'"${BITBUCKET_BRANCH}"'"
+#             }
+#           },
+#           "destination": {
+#             "branch": {
+#               "name": "develop"
+#             }
+#           }
+#         }'
+# fi
 
