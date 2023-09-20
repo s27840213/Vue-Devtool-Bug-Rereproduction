db23b76c08c78283aef97e9ce8fb39006331e7b0
Alan Chang <nuphoto.alan@gmail.com>
2023-09-20 10:22:57 +0800

    fix: fix the auth header

--- tools/bash/create_pull_request.sh
+++ tools/bash/create_pull_request.sh
@@ -8,7 +8,7 @@ curl --request POST \
     --url "https://api.bitbucket.org/2.0/repositories/mingchi/frontend-web/pullrequests" \
     --header "Accept: application/json" \
     --header "Content-Type: application/json" \
-    --header "Authorization: Basic '"${BITBUCKET_API_TOKEN}"'" \
+    --header "Authorization: Bearer '"${BITBUCKET_API_TOKEN}"'" \
     --data '{
       "title": "Pull request to develop by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
       "source": {
