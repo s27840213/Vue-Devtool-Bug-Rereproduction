9bb79de73c2b85994e3bf3fab62354d6d500f1d9
Alan Chang <nuphoto.alan@gmail.com>
2023-09-20 14:59:45 +0800

    fix: remove the double quote in pr script

--- tools/bash/create_pull_request.sh
+++ tools/bash/create_pull_request.sh
@@ -5,13 +5,13 @@ apt-get -y install curl jq
 export BITBUCKET_TRIGGERER_USERNAME=$(curl -X GET -g "https://api.bitbucket.org/2.0/users/${BITBUCKET_STEP_TRIGGERER_UUID}" | jq --raw-output '.display_name')
 
 echo "create pull request to develop"
-echo "'"${BITBUCKET_BRANCH}"'"
-echo "'"${BITBUCKET_API_TOKEN}"'"
+echo "${BITBUCKET_BRANCH}"
+echo "${BITBUCKET_API_TOKEN}"
 curl --request POST \
     --url "https://api.bitbucket.org/2.0/repositories/mingchi/frontend-web/pullrequests" \
     --header "Accept: application/json" \
     --header "Content-Type: application/json" \
-    --header "Authorization: Bearer '"${BITBUCKET_API_TOKEN}"'" \
+    --header "Authorization: Bearer ${BITBUCKET_API_TOKEN}" \
     --data '{
       "title": "Pull request to develop by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
       "source": {
