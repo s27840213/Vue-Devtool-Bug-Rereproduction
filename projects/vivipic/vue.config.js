145126e9c93e3e59306d8d5f920622795a2578a5
Alan Chang <nuphoto.alan@gmail.com>
2023-09-22 17:32:45 +0800

    fix: move prerender static files to production env

--- projects/vivipic/vue.config.js
+++ projects/vivipic/vue.config.js
@@ -203,24 +203,19 @@ module.exports = defineConfig({
         if (err) console.error(err)
       })
     }
-    console.log(argv.PRERENDER)
-    // if (argv.PRERENDER) {
-    //   console.log('start prerender')
     //   // Tell Vue (CLI 3) to provide this file to Pre-SPA:
-    console.log(process.env.NODE_ENV)
     if (process.env.NODE_ENV === 'production') {
       config.plugin('html').tap((args) => {
         args[0].template = path.join(__dirname, 'public', 'index.html')
         args[0].filename = 'app.html'
         return args
       })
+      config.plugin('copy-plugin').use(CopyPlugin, [
+        {
+          patterns: [resolve('prerender')],
+        },
+      ])
     }
-
-    config.plugin('copy-plugin').use(CopyPlugin, [
-      {
-        patterns: [resolve('prerender')],
-      },
-    ])
 
     //   config.plugin('prerender').use(PrerenderSPAPlugin, [
     //     {
