66d601a72d1245a69c8661591a31aad494cf4c6d
TingAnYen <nuphoto.daniel@gmail.com>
2023-09-22 11:04:05 +0800

    fix: missing crossOrigin

--- projects/vivipic/src/components/editor/global/NuBgImage.vue
+++ projects/vivipic/src/components/editor/global/NuBgImage.vue
@@ -156,7 +156,7 @@ export default defineComponent({
       if (this.isAdjustImage) {
         imageUtils.imgLoadHandler(this.src, (img) => {
           this.handleIsTransparent(img)
-        })
+        }, { crossOrigin: true })
       }
     }
   },
