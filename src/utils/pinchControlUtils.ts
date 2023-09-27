65e8294b855d6b6e08f05fecdbfc19a30ed7ef25
steve <nuphoto.steve@gmail.com>
2023-09-27 23:23:19 +0800

    feat: add line feature

--- src/utils/pinchControlUtils.ts
+++ src/utils/pinchControlUtils.ts
@@ -1,12 +1,14 @@
-import { IImage, ILayer } from '@/interfaces/layer'
+import { IImage, ILayer, IShape } from '@/interfaces/layer'
 import { ISize } from '@/interfaces/math'
 import store from '@/store'
 import { ILayerInfo } from '@/store/types'
+import controlUtils from '@/utils/controlUtils'
 import generalUtils from '@/utils/generalUtils'
 import groupUtils from '@/utils/groupUtils'
 import layerUtils from '@/utils/layerUtils'
 import { MovingUtils } from '@/utils/movingUtils'
 import pointerEvtUtils from '@/utils/pointerEvtUtils'
+import shapeUtils from '@/utils/shapeUtils'
 import vivistickerUtils from '@/utils/vivistickerUtils'
 import { AnyTouchEvent } from 'any-touch'
 
@@ -64,6 +66,7 @@ export default class PinchControlUtils {
       if (store.getters['vivisticker/getControllerHidden']) {
         vivistickerUtils.showController()
       }
+
       this.init = {
         pos: { x: e.x, y: e.y },
         layerPos: { x: this.config.styles.x, y: this.config.styles.y },
@@ -78,6 +81,9 @@ export default class PinchControlUtils {
           imgX: (this.config as IImage).styles.imgX,
           imgY: (this.config as IImage).styles.imgY,
         }
+      } else if (this.config.type === 'shape' && this.config.category === 'D') {
+        const { angle } = shapeUtils.lineDimension((this.config as any).point)
+        this.init.rotate = angle / Math.PI * 180
       }
     }
 
@@ -109,20 +115,32 @@ export default class PinchControlUtils {
       x: movingTranslate.x + compensateTranslate.x,
       y: movingTranslate.y + compensateTranslate.y
     }
-    const styles = {
+    let styles = {
       scale: newScale,
       width: newSize.width,
       height: newSize.height,
       rotate: evtAngle + this.init.rotate,
       x: this.init.layerPos.x + totalTranslate.x,
       y: this.init.layerPos.y + totalTranslate.y
-    } as { [key: string]: number }
+    } as { [key: string]: number | undefined }
+
     if (this.config.type === 'image' && this.init.img) {
       styles.imgWidth = this.init.img.imgWidth * evtScale
       styles.imgHeight = this.init.img.imgHeight * evtScale
       styles.imgX = this.init.img.imgX * evtScale
       styles.imgY = this.init.img.imgY * evtScale
+    } else if (this.config.type === 'shape' && this.config.category === 'D') {
+      const shape = this.config as IShape
+      const { point, dx, dy } = shapeUtils.lineCenterRotate(shape.point as number[], evtAngle + this.init.rotate, shape.size?.[0] ?? 1, false)
+      // const newPoint = [0, 0, point[2] * e.deltaScale, point[3] * e.deltaScale]
+      // const [scaleDX, scaleDY] = [point[2] - newPoint[2], point[3] - newPoint[3]]
+      styles = {
+        x: this.config.styles.x + e.deltaX + dx,
+        y: this.config.styles.y + e.deltaY + dy
+      }
+      controlUtils.updateShapeLinePoint(this.layerInfo.pageIndex, this.layerInfo.layerIndex, point)
     }
+
     if (this.layerInfo.pageIndex !== -1 && this.layerInfo.layerIndex !== -1) {
       layerUtils.updateLayerStyles(this.layerInfo.pageIndex, this.layerInfo.layerIndex, styles)
     }
