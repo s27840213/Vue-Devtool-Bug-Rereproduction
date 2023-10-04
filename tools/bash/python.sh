7788d34ba994c987eef35998a9a2f0f6fd1c8bd8
TingAnYen <nuphoto.daniel@gmail.com>
2023-10-04 11:13:55 +0800

    fix: add hint for Windows RDs to run python.sh

--- tools/bash/python.sh
+++ tools/bash/python.sh
@@ -1,3 +1,4 @@
+# For Windows, run this command first: pnpm config set script-shell "C:\\Program Files\\git\\bin\\bash.exe"
 # Check if the output matches the pattern
 if [[ $(uname) == "Darwin" ]]; then
   python3 $@
