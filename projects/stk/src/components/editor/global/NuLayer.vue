a06320898472214d124dfbf35defffd54573bffb
steve <ste14732@gmail.com>
2023-10-17 11:35:39 +0800

    fix: svg viewbox for mobile contentScaleRatio

--- projects/stk/src/components/editor/global/NuLayer.vue
+++ projects/stk/src/components/editor/global/NuLayer.vue
@@ -447,14 +447,33 @@ export default defineComponent({
 
       if (!isImgType) {
         const _f = this.contentScaleRatio * (this.$isTouchDevice() ? this.scaleRatio * 0.01 : 1)
-        const transform = `scale(${scale * (type === 'text' ? _f : 1)}) ${scaleX !== 1 ? `scaleX(${scaleX})` : ''} ${scaleY !== 1 ? `scaleY(${scaleY})` : ''}`
+        let transform
+        if (type === 'text' || (type === 'shape' && this.config.category !== 'D')) {
+          transform = `scale(${scale * _f}) ${scaleX !== 1 ? `scaleX(${scaleX})` : ''} ${scaleY !== 1 ? `scaleY(${scaleY})` : ''}`
+        } else {
+          transform = `scale(${scale}) ${scaleX !== 1 ? `scaleX(${scaleX})` : ''} ${scaleY !== 1 ? `scaleY(${scaleY})` : ''}`
+        }
         if (transform !== 'scale(1)') {
-          if (this.config.type === LayerType.text) {
-            styles.width = `${this.config.styles.width / this.config.styles.scale}px`
-            styles.height = `${this.config.styles.height / this.config.styles.scale}px`
-          } else {
-            styles.width = `${this.config.styles.initWidth * _f}px`
-            styles.height = `${this.config.styles.initHeight * _f}px`
+          switch (this.config.type) {
+            case LayerType.shape: {
+              if (this.config.category === 'D') {
+                styles.width = `${this.config.styles.initWidth * _f}px`
+                styles.height = `${this.config.styles.initHeight * _f}px`
+              } else {
+                styles.width = `${this.config.styles.initWidth}px`
+                styles.height = `${this.config.styles.initHeight}px`
+              }
+              break
+            }
+            case LayerType.text: {
+              styles.width = `${this.config.styles.width / this.config.styles.scale}px`
+              styles.height = `${this.config.styles.height / this.config.styles.scale}px`
+              break
+            }
+            default: {
+              styles.width = `${this.config.styles.initWidth * _f}px`
+              styles.height = `${this.config.styles.initHeight * _f}px`
+            }
           }
           styles.transform = transform
         }
