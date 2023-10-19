d492aa960a7efc0e1a78c09543cf3307bad781d9
Nathan <nuphoto.nathan@gmail.com>
2023-10-19 15:56:09 +0800

    fix: add cm @i18n to solve build error

--- projects/cm/src/i18n/index.ts
+++ projects/cm/src/i18n/index.ts
@@ -1,11 +1,14 @@
-import jp from '@i18n/jp.json'
 import tw from '@i18n/tw.json'
 import us from '@i18n/us.json'
-import i18n, { LocaleName as _LocaleName } from '@nu/vivi-lib/i18n'
+import { createI18n } from 'vue-i18n'
 
-i18n.global.setLocaleMessage('us', us)
-i18n.global.setLocaleMessage('tw', tw)
-i18n.global.setLocaleMessage('jp', jp)
-
-export type LocaleName = _LocaleName
-export default i18n
+export default createI18n({
+  legacy: false,
+  locale: 'us',
+  fallbackLocale: 'us',
+  messages: {
+    us,
+    zh: tw,
+  },
+  globalInjection: true,
+})
