375617f55f6da9da7622f1b4bbe100067a6c950e
Alan Chang <nuphoto.alan@gmail.com>
2023-10-27 09:58:10 +0800

    fix: move some components to vivi-lib

--- projects/cm/src/main.ts
+++ projects/cm/src/main.ts
@@ -3,12 +3,13 @@ import componentPlugin from '@nu/shared-lib/plugin'
 import libType from '@nu/shared-lib/types'
 import { initApp } from '@nu/vivi-lib/main'
 
-import '@nu/vivi-lib/css' // Import all CSS rules from vivi-lib
+import '@nu/vivi-lib/css'; // Import all CSS rules from vivi-lib
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 import App from './App.vue'
 import i18n from './i18n'
 import router from './router'
+import vuex from './vuex'
 
 const svgs = import.meta.glob('./assets/icon/**/*.svg', { eager: true })
 const app = initApp(createApp(App))
@@ -23,6 +24,7 @@ keepVar(libType)
 keepVar(svgs)
 
 app.use(createPinia())
+app.use(vuex)
 app.use(router)
 app.use(i18n)
 app.use(componentPlugin)
