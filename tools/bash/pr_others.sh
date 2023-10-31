2f45394a6fe27e795a39931575f8825811247f2d
Alan Chang <nuphoto.alan@gmail.com>
2023-10-31 10:58:16 +0800

    fix: modify the pipeline yml settings for charmix

--- tools/bash/pr_others.sh
+++ tools/bash/pr_others.sh
@@ -46,6 +46,27 @@ then
             }
           }
         }'
+elif [[ "$BITBUCKET_BRANCH" == "qa-cm" ]]
+then
+    echo "create pull request to main/cm"
+    curl --request POST \
+        --url "https://api.bitbucket.org/2.0/repositories/mingchi/frontend-web/pullrequests" \
+        --header "Accept: application/json" \
+        --header "Content-Type: application/json" \
+        --header "Authorization: Bearer ${BITBUCKET_API_TOKEN}" \
+        --data '{
+          "title": "Pull request to main/cm by '"${BITBUCKET_TRIGGERER_USERNAME}"'",
+          "source": {
+            "branch": {
+              "name": "'"${BITBUCKET_BRANCH}"'"
+            }
+          },
+          "destination": {
+            "branch": {
+              "name": "main/cm"
+            }
+          }
+        }'
 # for feature, test, bugfix branch
 else
     echo "create pull request to develop"
