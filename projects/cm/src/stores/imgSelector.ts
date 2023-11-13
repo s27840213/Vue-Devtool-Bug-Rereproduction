d8eaf3158a356bfaa6f6ee01268123499b5d3c34
Nathan <nuphoto.nathan@gmail.com>
2023-11-13 15:56:03 +0800

    feat(cm): add replace img

--- projects/cm/src/stores/imgSelector.ts
+++ projects/cm/src/stores/imgSelector.ts
@@ -2,16 +2,24 @@ import { defineStore } from 'pinia'
 
 export const useImgSelectorStore = defineStore('imgSelector', () => {
   const requireImgNum = ref(0)
+  const replaceImgFlag = ref(false)
+  const tabIndex = ref(0)
 
-  const setRequireImgNum = (value: number) => {
+  const setRequireImgNum = (value: number, { replace = false } = {}) => {
+    if (replace && value !== 1) {
+      console.warn('Value should be 1 when replace is true.')
+    }
     requireImgNum.value = value
+    replaceImgFlag.value = replace
   }
 
   const showImgSelector = computed(() => requireImgNum.value > 0)
 
   return {
+    tabIndex,
     requireImgNum,
-    setRequireImgNum,
+    replaceImgFlag,
     showImgSelector,
+    setRequireImgNum,
   }
 })
