113df09977efcdd730e762594d5bd4678d4771e9
steve <ste14732@gmail.com>
2023-11-13 10:19:19 +0800

    test: golden

--- packages/vivi-lib/src/utils/imageUtils.ts
+++ packages/vivi-lib/src/utils/imageUtils.ts
@@ -25,7 +25,7 @@ import frameDefaultImg from '@img/svg/frame.svg'
 import { AxiosPromise } from 'axios'
 import { cloneDeep, findLastIndex } from 'lodash'
 
-const APP_VER_FOR_REFRESH_CACHE = 'v778'
+const APP_VER_FOR_REFRESH_CACHE = 'v665'
 
 class ImageUtils {
   get imageSizeMap(): { [key: string]: number } {
