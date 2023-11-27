7e0b1d4ae1d9f04586686b57dcb1511f2decb15f
dantnc <fad11204@yahoo.com.tw>
2023-11-27 11:04:25 +0800

    fix(cm): if index is out of range, use '' as src

--- projects/cm/src/views/EditorView.vue
+++ projects/cm/src/views/EditorView.vue
@@ -59,7 +59,7 @@ div(class="w-full h-full grid grid-cols-1 grid-rows-[auto,minmax(0,1fr)]")
         img(
           v-if="inGenResultState"
           class="h-full object-cover"
-          :src="currGenResultIndex === -1 ? initImgSrc : generatedResults[currGenResultIndex].url")
+          :src="currImgSrc")
         template(v-else)
           nu-page(
             class="z-page"
@@ -92,7 +92,7 @@ div(class="w-full h-full grid grid-cols-1 grid-rows-[auto,minmax(0,1fr)]")
         img(
           class="result-showcase__card result-showcase__card--back absolute top-0 left-0"
           :class="{ 'is-flipped': !showVideo }"
-          :src="currGenResultIndex === -1 ? initImgSrc : generatedResults[currGenResultIndex].url")
+          :src="currImgSrc")
         img(
           class="result-showcase__card result-showcase__card--front"
           :class="{ 'is-flipped': showVideo }"
@@ -100,7 +100,7 @@ div(class="w-full h-full grid grid-cols-1 grid-rows-[auto,minmax(0,1fr)]")
         //- img(
         //-   class="result-showcase__card result-showcase__card--front"
         //-   :class="{ 'is-flipped': !showVideo }"
-        //-   :src="currGenResultIndex === -1 ? initImgSrc : generatedResults[currGenResultIndex].url")
+        //-   :src="currImgSrc")
         //- div(class="result-showcase__card result-showcase__card--back" :class="{ 'is-flipped': showVideo }")
         //-   img(
         //-     v-if="!isVideoGened"
@@ -287,6 +287,12 @@ const handleNextAction = function () {
     }
   }
 }
+
+const currImgSrc = computed(() => {
+  return currGenResultIndex.value === -1
+    ? initImgSrc.value
+    : generatedResults.value[currGenResultIndex.value]?.url ?? ''
+})
 
 const useStep = useSteps()
 const { undo, redo, isInFirstStep, isInLastStep } = useStep
