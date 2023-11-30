c087e97a2d0c428d8cb5414033b04dd1c8183e08
steve <ste14732@gmail.com>
2023-11-30 23:42:54 +0800

    fix: page zooming precision fixed/optimized

--- packages/vivi-lib/src/utils/pagePinchUtils.ts
+++ packages/vivi-lib/src/utils/pagePinchUtils.ts
@@ -9,7 +9,9 @@ class pagePinchUtils {
   private initPagePos = { x: 0, y: 0 }
   private initPinchPos = { x: 0, y: 0 }
   private initPinchScale = -1
+  private initPageScale = -1
   private editorView: HTMLElement
+  private translationRatio = { x: 0, y: 0 }
   private get page(): IPage { return pageUtils.getCurrPage }
   private get contentScaleRatio(): number { return this.page.contentScaleRatio}
   private get pageScaleRatio(): number { return store.getters.getPageScaleRatio }
@@ -29,48 +31,46 @@ class pagePinchUtils {
     this.initPagePos.y = this.page.y
     this.initPinchPos = { x: e.x, y: e.y }
     this.initPinchScale = e.scale
+    this.initPageScale = this.pageScaleRatio
+    this.translationRatio = this.getTranslationRatio(e, this.editorView)
+    console.warn('initpinch pos', this.initPagePos, this.initPinchPos)
     store.commit('SET_isPageScaling', true)
     store.commit('mobileEditor/SET_isPinchingEditor', true)
 
-    const pageRenderSize = { width: this.page.width * this.page.contentScaleRatio, height: this.page.height * this.page.contentScaleRatio }
-    // const posInConfig = {
-    //   x: (e.x - editorUtils.mobileCenterPos.x + pageRenderSize.width * 0.5) / this.page.contentScaleRatio - this.config.styles.imgX,
-    //   y: (e.y - editorUtils.mobileCenterPos.y + pageRenderSize.height * 0.5) / this.page.contentScaleRatio - this.config.styles.imgY
-    // }
-    // this.translationRatio = {
-    //   x: -posInConfig.x / this.config.styles.imgWidth,
-    //   y: -posInConfig.y / this.config.styles.imgHeight
-    // }
     return this.initPinchPos
   }
 
-  private getTranslationRatio(e: AnyTouchEvent, el: HTMLElement) {
-    const rect = el.getBoundingClientRect()
-    // console.log('rect', rect)
-    // console.log(e.x, e.y)
+  private getTranslationRatio(e: AnyTouchEvent, windowEl: HTMLElement) {
+    const rect = windowEl.getBoundingClientRect()
     return {
-      x: (e.x - rect.left) / rect.width,
-      y: (e.y - rect.top) / rect.height
-      // x: (e.x - rect.left) / (rect.width - e.x + rect.left),
-      // y: (e.y - rect.top) / (rect.height - e.y + rect.top)
+      // x: 0.5,
+      // y: 0.5
+      x: ((e.x - rect.left) - this.page.x) / (this.page.width * this.pageScaleRatio * this.contentScaleRatio * 0.01),
+      y: ((e.y - rect.top) - this.page.y) / (this.page.height * this.pageScaleRatio * this.contentScaleRatio * 0.01)
     }
   }
 
   private pinchMove(e: AnyTouchEvent) {
     const { page, contentScaleRatio } = this
-    const newScaleRatio = this.initPinchScale * e.scale
+    const newScaleRatio = e.scale
     // size difference via pinching
     const sizeDiff = {
-      width: (newScaleRatio - this.initPinchScale) * (page.width * contentScaleRatio),
-      height: (newScaleRatio - this.initPinchScale) * (page.height * contentScaleRatio)
+      width: (newScaleRatio - 1) * (page.width * contentScaleRatio * this.initPageScale * 0.01),
+      height: (newScaleRatio - 1) * (page.height * contentScaleRatio * this.initPageScale * 0.01)
     }
-    console.log(this.getTranslationRatio(e, this.editorView))
-    store.commit('mobileEditor/UPDATE_pinchScale', e.scale * this.initPinchScale)
+
+    console.log(this.initPagePos.x - this.translationRatio.x * sizeDiff.width)
+    console.log(this.initPagePos.x, this.translationRatio.x, sizeDiff.width)
+    pageUtils.updatePagePos(layerUtils.pageIndex, {
+      x: this.initPagePos.x - this.translationRatio.x * sizeDiff.width,
+      y: this.initPagePos.y - this.translationRatio.y * sizeDiff.height
+    })
+    store.commit('mobileEditor/UPDATE_pinchScale', e.scale)
     // pageUtils.updatePageProps()
   }
 
   private handleEdging(e: AnyTouchEvent) {
-    const newPageScaleRatio = e.scale * this.initPinchScale * this.pageScaleRatio
+    const newPageScaleRatio = store.state.mobileEditor.pinchScale * this.initPageScale
 
     if (newPageScaleRatio < 100) {
       store.commit('SET_pageScaleRatio', 100)
@@ -78,17 +78,15 @@ class pagePinchUtils {
         x: this.page.initPos.x,
         y: this.page.initPos.y
       })
-      console.log('page init pos', this.page.initPos)
     } else {
       // no need to edging the page
       store.commit('SET_pageScaleRatio', newPageScaleRatio)
     }
-    console.log('SET_pageScaleRatio', newPageScaleRatio)
   }
 
   private pinchEnd(e: AnyTouchEvent) {
-    this.resetState()
     this.handleEdging(e)
+    this.resetState()
     console.log('pinch end', e.scale)
   }
 
