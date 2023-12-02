3dd60af3c1729174f0ad50a1148080cdcb79510e
steve <ste14732@gmail.com>
2023-12-03 00:18:51 +0800

    feat: add edging handling

--- packages/vivi-lib/src/utils/pagePinchUtils.ts
+++ packages/vivi-lib/src/utils/pagePinchUtils.ts
@@ -2,13 +2,15 @@ import { IPage } from "@/interfaces/page"
 import store from "@/store"
 import { AnyTouchEvent } from "any-touch"
 import layerUtils from "./layerUtils"
+import mathUtils from "./mathUtils"
 import pageUtils from "./pageUtils"
+
+const MAX_SCALE = 300
 
 class pagePinchUtils {
   private isPinchInit = false
   private initPagePos = { x: 0, y: 0 }
   private initPinchPos = { x: 0, y: 0 }
-  private initPinchScale = -1
   private initPageScale = -1
   private editorView: HTMLElement
   private translationRatio = { x: 0, y: 0 }
@@ -30,7 +32,6 @@ class pagePinchUtils {
     this.initPagePos.x = this.page.x
     this.initPagePos.y = this.page.y
     this.initPinchPos = { x: e.x, y: e.y }
-    this.initPinchScale = e.scale
     this.initPageScale = this.pageScaleRatio
     this.translationRatio = this.getTranslationRatio(e, this.editorView)
     console.warn('initpinch pos', this.initPagePos, this.initPinchPos)
@@ -72,16 +73,71 @@ class pagePinchUtils {
     // pageUtils.updatePageProps()
   }
 
-  private handleEdging(e: AnyTouchEvent) {
-    const newPageScaleRatio = store.state.mobileEditor.pinchScale * this.initPageScale
+  private getEdgeLimit(pageScaleRatio: number) {
+    const { page, contentScaleRatio } = this
+    return {
+      left: 0,
+      right: (pageScaleRatio - 100) * page.width * contentScaleRatio * 0.01,
+      top: 0,
+      bottom: (pageScaleRatio - 100) * page.height * contentScaleRatio * 0.01
+    }
+  }
 
+  private handleEdging(e: AnyTouchEvent) {
+    const { page, initPageScale, contentScaleRatio } = this
+    const newPageScaleRatio = store.state.mobileEditor.pinchScale * initPageScale
+    const edgeLimit = this.getEdgeLimit(newPageScaleRatio)
+    const isReachLeft = page.x > edgeLimit.left
+    const isReachRight = edgeLimit.right < -page.x
+    const isReachTop = page.y > edgeLimit.top
+    const isReachBottom = edgeLimit.bottom < -page.y
+    console.log(isReachLeft, isReachRight)
+
+    // case 1: page smaller than default size
     if (newPageScaleRatio < 100) {
+      console.warn(1)
       store.commit('SET_pageScaleRatio', 100)
       pageUtils.updatePagePos(layerUtils.pageIndex, {
         x: this.page.initPos.x,
         y: this.page.initPos.y
       })
+    // case 2: page bigger than maximum size
+    } else if (newPageScaleRatio > MAX_SCALE) {
+      console.warn(3)
+      const sizeDiff = {
+        width: (newPageScaleRatio - MAX_SCALE) * (page.width * contentScaleRatio * 0.01),
+        height: (newPageScaleRatio - MAX_SCALE) * (page.height * contentScaleRatio * 0.01)
+      }
+      const newPos = {
+        x: page.x + sizeDiff.width * this.translationRatio.x,
+        y: page.y + sizeDiff.height * this.translationRatio.y
+      }
+      const newEdgeLimit = this.getEdgeLimit(MAX_SCALE)
+      pageUtils.updatePagePos(layerUtils.pageIndex, {
+        x: mathUtils.clamp(newPos.x, newEdgeLimit.right, newEdgeLimit.left),
+        y: mathUtils.clamp(newPos.y, newEdgeLimit.bottom, newEdgeLimit.top)
+      })
+      store.commit('SET_pageScaleRatio', MAX_SCALE)
+    // case 3: page size proper but reach edges
+    } else if (isReachLeft || isReachRight || isReachTop || isReachBottom) {
+      console.warn(3)
+      const newPos = {
+        x: page.x,
+        y: page.y
+      }
+      if (isReachLeft) {
+        newPos.x = 0
+      } else if (isReachRight) {
+        newPos.x = -edgeLimit.right
+      }
+      if (isReachTop) {
+        newPos.y = 0
+      } else if (isReachBottom) {
+        newPos.y = -edgeLimit.bottom
+      }
+      pageUtils.updatePagePos(layerUtils.pageIndex, newPos)
     } else {
+      console.warn(4)
       // no need to edging the page
       store.commit('SET_pageScaleRatio', newPageScaleRatio)
     }
