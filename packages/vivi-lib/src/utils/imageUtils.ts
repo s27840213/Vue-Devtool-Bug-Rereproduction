0007d518b4d1ea4b74fde10b9302dc9347d9f403
TingAnYen <nuphoto.daniel@gmail.com>
2024-01-19 11:44:11 +0800

    fix: disable crossOrigin for all backendRendering cases

--- packages/vivi-lib/src/utils/imageUtils.ts
+++ packages/vivi-lib/src/utils/imageUtils.ts
@@ -1,10 +1,10 @@
 import imageApi from '@/apis/image-api'
 import {
-  IAssetPhoto,
-  IImageSize,
-  IPhotoItem,
-  IUserImageContentData,
-  isIAssetPhoto,
+IAssetPhoto,
+IImageSize,
+IPhotoItem,
+IUserImageContentData,
+isIAssetPhoto,
 } from '@/interfaces/api'
 import { ICoordinate } from '@/interfaces/frame'
 import { SrcObj } from '@/interfaces/gallery'
@@ -26,7 +26,7 @@ import { AxiosPromise } from 'axios'
 import { cloneDeep, findLastIndex } from 'lodash'
 import logUtils from './logUtils'
 
-const APP_VER_FOR_REFRESH_CACHE = 'v922'
+const APP_VER_FOR_REFRESH_CACHE = 'v1571'
 
 class ImageUtils {
   get imageSizeMap(): { [key: string]: number } {
@@ -48,7 +48,7 @@ class ImageUtils {
     const { error, crossOrigin = true } = options || {}
     return new Promise<T>((resolve, reject) => {
       const image = new Image()
-      if (crossOrigin && !src.includes('data:image/png;base64')) {
+      if (crossOrigin && store.getters['user/getUserId'] !== 'backendRendering' && !src.includes('data:image/png;base64')) {
         image.crossOrigin = 'anonymous'
       }
       image.onload = () => resolve(cb(image))
